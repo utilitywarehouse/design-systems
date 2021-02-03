6f490b3937fac60f0e7159f00e41985f53711692
Lewis Nelson <lewis@lewnelson.com>
2021-02-03 12:21:46 +0000

    chore(material): updated story imports

--- packages/material/stories/components/Button.stories.tsx
+++ packages/material/stories/components/Button.stories.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import base from "paths.macro";
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from "@storybook/react/types-6-0";
+import { Story, Meta } from "@storybook/react";
 
 import { Button, ButtonProps } from "./Button";
 import { withBackground, BackgroundColor } from "../../src";
