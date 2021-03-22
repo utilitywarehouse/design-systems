0ed500b179f204da0aff9b80d27aae1d091ca34a
Lewis Nelson <lewis@lewnelson.com>
2021-03-22 12:44:55 +0000

    refactor(design tokens): refactored spacing on design tokens

--- packages/design-tokens/src/index.ts
+++ packages/design-tokens/src/index.ts
@@ -1,6 +1,6 @@
 export { colors, Colors } from "./colors";
 export { fontWeights, FontWeights, fonts, Fonts } from "./typography";
 export { breakpoints, Breakpoints } from "./breakpoints";
-export { spacing, Spacing, base as spacingBase } from "./spacing";
+export { base as spacingBase } from "./spacing";
 
 export * as helpers from "./helpers";
