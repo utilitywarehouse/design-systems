b96b28aa9bcbd6113bf76837b1b44ded98146dad
Lewis Nelson <lewis@lewnelson.com>
2021-03-26 15:22:27 +0000

    feat(material): grid component adhering to dsm

---
+++ packages/material/src/components/Grid.tsx
@@ -0,0 +1,219 @@
+import React from "react";
+import { GridDirection, GridWrap } from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import { Breakpoint } from "@utilitywarehouse/customer-ui-theme";
+import { useMediaQuery, useTheme } from "../";
+import { MuiTheme } from "../lib/theme";
+
+type GridContextValue = StyleProps;
+
+const defaultGridContextValue: GridContextValue = {
+  container: true,
+  item: false,
+  direction: {
+    mobile: "row",
+    tablet: "row",
+    desktop: "row",
+  },
+  wrap: {
+    mobile: "wrap",
+    tablet: "wrap",
+    desktop: "wrap",
+  },
+  mobile: "auto",
+  tablet: "auto",
+  desktop: "auto",
+  breakpoint: "desktop",
+};
+
+const GridContext = React.createContext<GridContextValue>(
+  defaultGridContextValue
+);
+
+enum MaxColumns {
+  mobile = 4,
+  tablet = 8,
+  desktop = 12,
+}
+
+type GridSizeAuto = "auto";
+type MobileGridSize = GridSizeAuto | 1 | 2 | 3 | 4;
+
+type TabletGridSize = MobileGridSize | 5 | 6 | 7 | 8;
+type DesktopGridSize = TabletGridSize | 9 | 10 | 11 | 12;
+
+type Direction = {
+  [key in Breakpoint]: GridDirection;
+};
+
+type Wrap = {
+  [key in Breakpoint]: GridWrap;
+};
+
+export type GridProps = {
+  container?: boolean;
+  item?: boolean;
+  direction?: {
+    [key in Breakpoint]?: GridDirection;
+  };
+  wrap?: {
+    [key in Breakpoint]?: GridWrap;
+  };
+  mobile?: MobileGridSize;
+  tablet?: TabletGridSize;
+  desktop?: DesktopGridSize;
+};
+
+type StyleProps = {
+  container: boolean;
+  item: boolean;
+  direction: Direction;
+  wrap: Wrap;
+  mobile: MobileGridSize;
+  tablet: TabletGridSize;
+  desktop: DesktopGridSize;
+  breakpoint: Breakpoint;
+};
+
+const toPercentage = (n: number) => `${n * 100}%`;
+
+const useStyles = makeStyles<MuiTheme, StyleProps>((theme: MuiTheme) => ({
+  root: {
+    width: "100%",
+    overflow: "hidden",
+  },
+  container: (props) => {
+    if (!props.container) {
+      return {};
+    }
+
+    const styles = {
+      width: `calc(100% + ${theme.spacing(3)})`,
+      display: "flex",
+      boxSizing: "border-box",
+      flexDirection: props.direction[props.breakpoint],
+      flexWrap: props.wrap[props.breakpoint],
+      margin: theme.spacing(-1.5),
+    };
+
+    return styles;
+  },
+  item: (props) => {
+    if (props.container) {
+      return {};
+    }
+
+    const size =
+      props[props.breakpoint] === "auto"
+        ? 1
+        : (props[props.breakpoint] as number);
+
+    const styles: Record<string, string | number> = {
+      width: "100%",
+      boxSizing: "border-box",
+      padding: theme.spacing(1.5),
+      flex: size,
+    };
+
+    const layout = props.direction[props.breakpoint].match(/^row/)
+      ? "row"
+      : "column";
+
+    if (layout === "row" && props[props.breakpoint] !== "auto") {
+      styles.flexBasis = toPercentage(size / MaxColumns[props.breakpoint]);
+      styles.maxWidth = toPercentage(size / MaxColumns[props.breakpoint]);
+      styles.width = toPercentage(size / MaxColumns[props.breakpoint]);
+    }
+
+    return styles;
+  },
+}));
+
+const Grid: React.FunctionComponent<GridProps> = ({
+  mobile = "auto",
+  tablet = "auto",
+  desktop = "auto",
+  container = false,
+  item = false,
+  direction,
+  wrap,
+  children,
+}) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("mobile"));
+  const isTablet = useMediaQuery(theme.breakpoints.between("mobile", "tablet"));
+  const containerStyleProps = React.useContext(GridContext);
+
+  const resolvedDirection = React.useMemo<Direction>(() => {
+    if (item) {
+      return containerStyleProps.direction;
+    }
+
+    const resolvedDirection = direction ?? {};
+    const keys: Breakpoint[] = ["desktop", "mobile", "tablet"];
+    keys.forEach((key) => {
+      resolvedDirection[key] = resolvedDirection[key] ?? "row";
+    });
+
+    return resolvedDirection as Direction;
+  }, [direction, containerStyleProps, item]);
+
+  const resolvedWrap = React.useMemo<Wrap>(() => {
+    if (item) {
+      return containerStyleProps.wrap;
+    }
+
+    const resolvedWrap = wrap ?? {};
+    const keys: Breakpoint[] = ["desktop", "mobile", "tablet"];
+    keys.forEach((key) => {
+      resolvedWrap[key] = resolvedWrap[key] ?? "wrap";
+    });
+
+    return resolvedWrap as Wrap;
+  }, [wrap, containerStyleProps, item]);
+
+  const styleProps = React.useMemo<StyleProps>(() => {
+    return {
+      mobile,
+      tablet,
+      desktop,
+      container,
+      item,
+      direction: resolvedDirection,
+      wrap: resolvedWrap,
+      breakpoint: (isMobile
+        ? "mobile"
+        : isTablet
+        ? "tablet"
+        : "desktop") as Breakpoint,
+    };
+  }, [
+    mobile,
+    tablet,
+    desktop,
+    container,
+    item,
+    resolvedDirection,
+    resolvedWrap,
+    isMobile,
+    isTablet,
+  ]);
+
+  const classes = useStyles(styleProps);
+  const divClassName = container ? "container" : "item";
+  if (container) {
+    return (
+      <GridContext.Provider value={styleProps}>
+        <div className={classes.root}>
+          <div className={classes[divClassName]}>{children}</div>
+        </div>
+      </GridContext.Provider>
+    );
+  } else if (item) {
+    return <div className={classes[divClassName]}>{children}</div>;
+  } else {
+    return null;
+  }
+};
+
+export default Grid;
---
+++ packages/material/stories/components/Grid.stories.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import base from "paths.macro";
+import { Story, Meta } from "@storybook/react";
+
+import { Background, Box, Typography } from "../../src";
+import { Grid, GridProps } from "./Grid";
+import { BackdropLevel } from "@utilitywarehouse/customer-ui-theme";
+
+export default {
+  title: `${base}Grids`,
+  component: Grid,
+} as Meta;
+
+const getBox = (
+  text: string,
+  height?: string,
+  backgroundColor?: BackdropLevel
+) => (
+  <Background
+    backgroundColor={backgroundColor ?? "level1"}
+    width="100%"
+    height={height ?? "200px"}
+    textAlign="center"
+  >
+    <Box
+      flexDirection="column"
+      display="flex"
+      height="100%"
+      justifyContent="center"
+    >
+      <Typography>{text}</Typography>
+    </Box>
+  </Background>
+);
+
+const bindTemplate = () => {
+  const Template: Story<GridProps> = () => {
+    return (
+      <Background backgroundColor="level4" padding={3}>
+        <Grid container>
+          <Grid item desktop={8} mobile={4}>
+            {getBox("Account actions")}
+          </Grid>
+          <Grid item desktop={4} mobile={4}>
+            {getBox("Billing")}
+          </Grid>
+        </Grid>
+        <Box paddingTop={3}>
+          <Grid
+            container
+            direction={{ mobile: "column-reverse", tablet: "column-reverse" }}
+          >
+            <Grid item desktop={8} mobile={4}>
+              <Grid container>
+                <Grid item desktop={3} tablet={2} mobile={2}>
+                  {getBox("Quick link 1", "80px", "level0")}
+                </Grid>
+                <Grid item desktop={3} tablet={2} mobile={2}>
+                  {getBox("Quick link 2", "80px", "level0")}
+                </Grid>
+                <Grid item desktop={3} tablet={2} mobile={2}>
+                  {getBox("Quick link 3", "80px", "level0")}
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item desktop={4} mobile={4}>
+              {getBox("Recommended for you")}
+            </Grid>
+          </Grid>
+        </Box>
+      </Background>
+    );
+  };
+
+  return Template;
+};
+
+export const GridExample = bindTemplate();
+GridExample.storyName = "Grid";
