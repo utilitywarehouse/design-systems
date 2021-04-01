698ba60aa4f99988ae99f839800121b9de7250eb
Lewis Nelson <lewis@lewnelson.com>
2021-04-01 13:20:59 +0100

    fix(material): fixed generation of class prefixes for styles provider to persist across rerenders

--- packages/material/src/components/StylesProvider.tsx
+++ packages/material/src/components/StylesProvider.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StylesProviderProps } from "@material-ui/styles";
+import { GenerateId } from "jss";
 import { MuiStylesProvider, createGenerateClassName } from "..";
 
 export { StylesProviderProps } from "@material-ui/styles";
@@ -7,21 +8,34 @@ export { StylesProviderProps } from "@material-ui/styles";
 const StylesProvider: React.FunctionComponent<StylesProviderProps> = (
   props
 ) => {
+  const [generateClassName, setGenerateClassName] = React.useState<{
+    generateClassName: GenerateId;
+  }>();
+
   const getRandomString = () =>
     ((Math.random() * 1e8) ^ (Math.random() * 10)).toString(16);
 
-  const generateClassName = React.useMemo(() => {
+  const getGenerateClassName = (): GenerateId => {
     return createGenerateClassName({
       disableGlobal: true,
       productionPrefix: getRandomString(),
       seed: getRandomString(),
     });
+  };
+
+  React.useEffect(() => {
+    const generateClassName = getGenerateClassName();
+    setGenerateClassName({ generateClassName });
   }, []);
+
+  if (!generateClassName?.generateClassName) {
+    return null;
+  }
 
   return (
     <MuiStylesProvider
       injectFirst
-      generateClassName={generateClassName}
+      generateClassName={generateClassName.generateClassName}
       {...props}
     />
   );
