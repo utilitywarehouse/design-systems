fa8947b04c6876a904e469ff5f146b1c7d1ab92a
Rob Phoenix <rphoenix@uw.co.uk>
2021-10-20 12:32:06 +0100

    chore(material): remove paths.macro

--- packages/material/stories/utils.ts
+++
@@ -1,5 +0,0 @@
-import type { BackdropLevel } from "@utilitywarehouse/customer-ui-theme";
-
-export const backgroundLevels = [0, 1, 2, 3, 4, 5].map(
-  (level) => `level${level}` as BackdropLevel
-);
