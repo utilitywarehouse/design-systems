25f43e83bc531e6b5a1da2f3117b0175526dc24c
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2022-01-11 13:47:40 +0000

    chore: delete unused scripts (#314)

--- scripts/get-pr-number.js
+++
@@ -1,5 +0,0 @@
-// Resolves PR number from CircleCI CIRCLE_PULL_REQUEST environment variable
-const prURL = process.env.CIRCLE_PULL_REQUEST || "";
-const prNumber = parseInt(prURL.split("/").pop() || "");
-if (isNaN(prNumber)) process.exit(0);
-process.stdout.write(`${prNumber}`);
