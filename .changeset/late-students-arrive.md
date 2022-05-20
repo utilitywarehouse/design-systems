be7e40ff1b2eabf40c03ab0a57e972092c88a89f
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2022-05-20 15:35:55 +0100

    fix Grid component prop (#396)
    * fix Grid component prop
    
    * update changeset

---
+++ .changeset/late-students-arrive.md
@@ -0,0 +1,6 @@
+---
+"@utilitywarehouse/customer-ui-material": patch
+---
+
+Add missing `component` prop to `Grid`.
+https://github.com/mui/material-ui/issues/29191
--- packages/material/src/components/Grid.tsx
+++ packages/material/src/components/Grid.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MuiGrid, { GridProps as MuiGridProps } from "@mui/material/Grid";
+import { BoxProps as MuiBoxProps } from "@mui/material/Box";
 
 // Currently the MuiGrid component doesn't work with custom
 // breakpoints, so we need to let it know what columns we want for our
@@ -9,6 +10,7 @@ const DEFAULT_COLUMNS = { mobile: 4, tablet: 8, desktop: 12 };
 const DEFAULT_SPACING = { mobile: 2, tablet: 3, desktop: 3 };
 
 export interface GridProps extends MuiGridProps {
+  component?: MuiBoxProps["component"];
   desktop?: MuiGridProps["lg"];
   tablet?: MuiGridProps["lg"];
   mobile?: MuiGridProps["lg"];
