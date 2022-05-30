c27ed6fb446588b147db1e7c0c650cc311ee34c9
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2022-05-30 16:15:10 +0100

    improve Button stories (#403)

--- packages/material/stories/Button.stories.tsx
+++ packages/material/stories/Button.stories.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 
-import { Button } from "../src";
+import { Button, Stack } from "../src";
 import type { ButtonProps } from "../src";
 import { BackgroundStack } from "./utils";
+
+const sizes = ["regular", "large"] as const;
+const variants = ["contained", "outlined", "tertiary"] as const;
 
 export default {
   title: "Components/Button",
   component: Button,
   argTypes: {
+    forwardedRef: { table: { disable: true } },
     children: {
+      control: {
+        type: "text",
+      },
+    },
+    href: {
       control: {
         type: "text",
       },
@@ -24,12 +33,43 @@ export default {
     size: "regular",
     variant: "contained",
     children: "Button",
+    fullWidth: false,
+    disabled: false,
   },
 } as Meta;
 
-export const ButtonStory: Story<ButtonProps> = (args) => (
+export const ButtonKitchenSinkStory: Story<ButtonProps> = (args) => (
+  <BackgroundStack>
+    <Stack spacing={4}>
+      {variants.map((variant) => (
+        <Stack key={variant} direction="row" spacing={2} alignItems="center">
+          <>
+            {sizes.map((size) => (
+              <Button key={size} {...args} size={size} variant={variant} />
+            ))}
+            {sizes.map((size) => (
+              <Button key={size} size={size} variant={variant} disabled={true}>
+                {args.children}
+              </Button>
+            ))}
+          </>
+        </Stack>
+      ))}
+    </Stack>
+  </BackgroundStack>
+);
+ButtonKitchenSinkStory.storyName = "Kitchen Sink";
+ButtonKitchenSinkStory.argTypes = {
+  variant: { table: { disable: true } },
+  size: { table: { disable: true } },
+  disabled: { table: { disable: true } },
+  fullWidth: { table: { disable: true } },
+  href: { table: { disable: true } },
+};
+
+export const ButtonCustomStory: Story<ButtonProps> = (args) => (
   <BackgroundStack>
     <Button {...args} />
   </BackgroundStack>
 );
-ButtonStory.storyName = "Button";
+ButtonCustomStory.storyName = "Custom";
