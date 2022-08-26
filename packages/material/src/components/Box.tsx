256218b4b3ff0e990227428552b311f0c86f29de
Rob Phoenix <rphoenix@uw.co.uk>
2022-08-26 09:39:50 +0100

    update Box type

--- packages/material/src/components/Box.tsx
+++ packages/material/src/components/Box.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import MuiBox, { BoxProps } from "@mui/material/Box";
 
-const Box = React.forwardRef<unknown, BoxProps>(function Box(props, ref) {
+const Box: typeof MuiBox = React.forwardRef<unknown, BoxProps>(function Box(
+  props,
+  ref
+) {
   console.warn(
     `The Box system properties, used for styling, are deprecated in v2, and will be removed in v3. Please migrate to using the sx and styled utilities instead.`
   );
   return <MuiBox ref={ref} {...props} />;
-});
+}) as typeof MuiBox;
 
 export default Box;
 export type { BoxProps };
