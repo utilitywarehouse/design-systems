6b61b4de6dc87ce343de6e45214a1a6d82273725
Rob Phoenix <rphoenix@uw.co.uk>
2022-08-30 16:44:39 +0100

    update React import in Button

--- packages/material/src/components/Button.tsx
+++ packages/material/src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import MuiButton, { ButtonProps as MuiButtonProps } from "@mui/material/Button";
 import { Components } from "@mui/material/styles";
 import {
