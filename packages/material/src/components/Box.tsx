bdcf519f3c167f239db62f356cc383cd989e7d9e
Rob Phoenix <rphoenix@uw.co.uk>
2022-09-02 15:02:41 +0100

    update Box warning

--- packages/material/src/components/Box.tsx
+++ packages/material/src/components/Box.tsx
@@ -2,13 +2,16 @@ import * as React from "react";
 import MuiBox, { BoxProps } from "@mui/material/Box";
 
 const Box: typeof MuiBox = React.forwardRef<HTMLElement, BoxProps>(function Box(
-  props,
+  { sx, component, children, className, ...systemProps },
   ref
 ) {
-  console.warn(
-    `The Box system properties, used for styling, are deprecated in v2, and will be removed in v3. Please migrate to using the sx and styled utilities instead.`
-  );
-  return <MuiBox ref={ref} {...props} />;
+  const props = { sx, component, children, className };
+  if (Object.keys(systemProps).length > 0) {
+    console.warn(
+      `The Box system properties, used for styling, are deprecated in v2, and will be removed in v3. Please migrate to using the sx and styled utilities instead.`
+    );
+  }
+  return <MuiBox ref={ref} {...systemProps} {...props} />;
 }) as typeof MuiBox;
 
 export default Box;
