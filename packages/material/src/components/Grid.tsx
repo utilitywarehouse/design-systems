5f7ce4d63229bbae2b97fcb5defa2bcfb31c3984
Rob Phoenix <rphoenix@uw.co.uk>
2022-09-09 10:50:58 +0100

    update refs in Grid component

--- packages/material/src/components/Grid.tsx
+++ packages/material/src/components/Grid.tsx
@@ -1,30 +1,42 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import * as React from "react";
-import MuiGrid, { GridProps as MuiGridProps } from "@mui/material/Grid";
-import { BoxProps } from "./Box";
+import MuiGrid, { GridTypeMap } from "@mui/material/Grid";
+import {
+  OverridableComponent,
+  OverrideProps,
+} from "@mui/material/OverridableComponent";
+
+type TypeMap<
+  P = {},
+  D extends React.ElementType = GridTypeMap["defaultComponent"]
+> = {
+  props: GridTypeMap<P, D>["props"];
+  defaultComponent: D;
+};
+
+export type GridProps<
+  D extends React.ElementType = TypeMap["defaultComponent"],
+  P = {}
+> = OverrideProps<TypeMap<P, D>, D>;
 
 const DEFAULT_COLUMNS = { mobile: 4, tablet: 8, desktop: 12 };
 const DEFAULT_SPACING = { mobile: 2, tablet: 3, desktop: 3 };
 
-export interface GridProps extends MuiGridProps {
-  component?: BoxProps["component"];
-}
-
-const Grid = React.forwardRef<HTMLDivElement, GridProps>(
-  ({ columns = DEFAULT_COLUMNS, ...props }, ref) => {
-    if (props.container) {
-      return (
-        <MuiGrid
-          ref={ref}
-          columns={columns}
-          spacing={props.spacing || DEFAULT_SPACING}
-          {...props}
-        />
-      );
-    }
-    return <MuiGrid ref={ref} columns={columns} {...props} />;
+export const Grid = React.forwardRef(function Grid(
+  { columns = DEFAULT_COLUMNS, ...props },
+  ref
+) {
+  if (props.container) {
+    return (
+      <MuiGrid
+        ref={ref}
+        columns={columns}
+        spacing={props.spacing || DEFAULT_SPACING}
+        {...props}
+      />
+    );
   }
-);
-
-Grid.displayName = "Grid";
+  return <MuiGrid ref={ref} columns={columns} {...props} />;
+}) as OverridableComponent<TypeMap>;
 
 export default Grid;
