b013b6c9a5a8dc6fda2168e65b763fff9001ef14
Rob Phoenix <rphoenix@uw.co.uk>
2022-11-01 12:02:46 +0000

    remove theme prop from ThemeProvider

--- packages/react/src/components/ThemeProvider.tsx
+++ packages/react/src/components/ThemeProvider.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react';
-import { ThemeProvider as MuiThemeProvider, Theme } from '@mui/material/styles';
-import { deepmerge } from '@mui/utils';
-import { theme as defaultTheme } from '../theme';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { theme } from '../theme';
 import CssBaseline from '@mui/material/CssBaseline';
 import '@utilitywarehouse/uw-ui-fonts';
 
 export interface ThemeProviderProps {
-  theme?: Partial<Theme>;
   children?: React.ReactNode;
 }
 
-const ThemeProvider = ({ theme, children }: ThemeProviderProps) => {
-  // do we need to use `createTheme` here? do we even want to do this?
-  const themeWithOverrides = deepmerge(defaultTheme, theme) as Theme;
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
   return (
-    <MuiThemeProvider theme={themeWithOverrides}>
+    <MuiThemeProvider theme={theme}>
       <CssBaseline />
       {children}
     </MuiThemeProvider>
