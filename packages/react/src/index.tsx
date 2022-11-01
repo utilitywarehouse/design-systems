8660d8689ac89416617dd1422283ae87304c0b68
Rob Phoenix <rphoenix@uw.co.uk>
2022-11-01 11:57:13 +0000

    update theme palette

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -5,7 +5,7 @@ export type { BoxProps } from './components/Box';
 export { default as Background } from './components/Background';
 export type { BackgroundProps } from './components/Background';
 export { default as ThemeProvider } from './components/ThemeProvider';
-export { default as Typography, variantMapping } from './components/Typography';
+export { default as Typography, variantMapping, typographyClasses } from './components/Typography';
 export type { TypographyProps } from './components/Typography';
 
 export { default as CssBaseline } from '@mui/material/CssBaseline';
--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -2,6 +2,8 @@ import { type BackgroundColor, brandBackgroundColors } from '../types';
 
 export const globalClassPrefix = 'uw-web-ui';
 
+export const px = (value: string | number): string => `${value}px`;
+
 export const isBrandBackgroundColor = (backgroundColor: BackgroundColor): boolean => {
   return (brandBackgroundColors as ReadonlyArray<string>).includes(backgroundColor);
 };
