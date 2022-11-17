6982d3876e6006779410b2a5e858a8d3f438c6d4
Rob Phoenix <rphoenix@uw.co.uk>
2022-11-17 23:58:15 +0000

    add TextLink component

---
+++ packages/react/src/Link/Link.tsx
@@ -0,0 +1,10 @@
+import TextLink, { TextLinkProps } from '../TextLink';
+
+export type LinkProps = TextLinkProps;
+
+/**
+ * @deprecated Please use the TextLink component
+ */
+const Link = TextLink;
+
+export default Link;
