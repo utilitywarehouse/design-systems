ae428b98a7372602243ce4d4d6ce6a3e3806e3a8
Rob Phoenix <rphoenix@uw.co.uk>
2022-11-19 09:40:59 +0000

    update utils

---
+++ packages/react/src/utils/index.ts
@@ -0,0 +1,21 @@
+import { type BackgroundColor, inverseBackgroundColors } from '../types';
+
+export const globalClassPrefix = 'uw-web-ui';
+
+export const px = (value: string | number): string => `${value}px`;
+
+export const isInverseBackgroundColor = (backgroundColor: BackgroundColor): boolean => {
+  return (inverseBackgroundColors as ReadonlyArray<string>).includes(backgroundColor);
+};
+
+export const isHeadingVariant = (variant: string): boolean => {
+  const headingVariants = ['displayHeading', 'h1', 'h2', 'h3', 'h4'];
+  return headingVariants.includes(variant);
+};
+
+export const dataAttributes = {
+  variant: 'variant',
+  size: 'size',
+  inverse: 'on-inverse-background',
+  disableCapitalizeFirstLetter: 'disable-capitalize-first-letter',
+};
