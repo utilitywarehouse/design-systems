9a1a81ddce298acf6c2ad650ba9f3bbea8b7fd25
Rob Phoenix <rphoenix@uw.co.uk>
2022-11-21 17:49:21 +0000

    fix storybook

--- apps/storybook/.storybook/manager.js
+++ apps/storybook/.storybook/manager.js
@@ -1,13 +1,13 @@
-import { addons } from "@storybook/addons";
-import { create } from "@storybook/theming";
-import { version } from "../../../packages/react/package.json";
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming';
+import { version } from '../../../packages/uw-web-ui/package.json';
 
 const theme = create({
   brandTitle: `UW Web UI v${version}`,
 });
 
 addons.setConfig({
-  panelPosition: "right",
-  selectedPanel: "storybook/controls/panel",
+  panelPosition: 'right',
+  selectedPanel: 'storybook/controls/panel',
   theme,
 });
