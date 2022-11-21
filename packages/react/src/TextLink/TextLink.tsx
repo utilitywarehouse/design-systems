a9bdd8d907d7472bf6679c72caf3f19519475a7f
Rob Phoenix <rphoenix@uw.co.uk>
2022-11-21 12:57:28 +0000

    update TextLink

--- packages/react/src/TextLink/TextLink.tsx
+++ packages/react/src/TextLink/TextLink.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import MuiLink, { LinkTypeMap as MuiLinkTypeMap } from '@mui/material/Link';
-import { isBrandBackgroundColor, isHeadingVariant } from '../utils';
+import MuiLink, { LinkProps as MuiLinkProps } from '@mui/material/Link';
+import { isHeadingVariant, isInverseBackgroundColor } from '../utils';
 import { colors, transitions } from '@utilitywarehouse/customer-ui-design-tokens';
 import { styled } from '@mui/material/styles';
-import { useBackground } from './Background';
-import { OverridableComponent, OverrideProps } from '@mui/material/OverridableComponent';
+import { useBackground } from '../Background';
 
-const BaseLink = styled(MuiLink)(({ variant = 'body' }) => {
+const StyledTextLink = styled(MuiLink)(({ variant = 'body' }) => {
   const { backgroundColor } = useBackground();
   const getLinkColor = () => {
     if (variant === 'inherit') {
       return variant;
     }
-    if (isBrandBackgroundColor(backgroundColor)) {
+    if (isInverseBackgroundColor(backgroundColor)) {
       return colors.white;
     }
     if (isHeadingVariant(variant)) {
@@ -20,130 +19,35 @@ const BaseLink = styled(MuiLink)(({ variant = 'body' }) => {
     }
     return colors.midnight;
   };
-
+  const color = getLinkColor();
   return {
     transition: `${transitions.duration}ms ${transitions.easingFunction}`,
     transitionProperty: 'text-decoration, color, opacity',
     opacity: 1,
     cursor: 'pointer',
-    ...(variant === 'inherit' && { textTransform: 'inherit' }),
     '&.MuiLink-root': {
-      color: getLinkColor(),
+      color,
     },
-  };
-});
-
-const StyledLink = styled(BaseLink)({
-  textDecoration: 'underline',
-  textDecorationThickness: 2,
-  textUnderlineOffset: 4,
-  textDecorationColor: colors.cyan,
-  '&:hover': {
-    opacity: 0.5,
-  },
-});
-
-type defaultComponent = 'a';
-
-export type CustomLinkProps<D extends React.ElementType = defaultComponent, P = {}> = Omit<
-  MuiLinkTypeMap<P, D>['props'],
-  'color' | 'underline'
->;
-
-interface LinkTypeMap<D extends React.ElementType = defaultComponent, P = {}> {
-  props: CustomLinkProps<D, P>;
-  defaultComponent: D;
-}
-
-export type LinkProps<D extends React.ElementType = defaultComponent, P = {}> = OverrideProps<
-  LinkTypeMap<D, P>,
-  D
->;
-
-const Link = React.forwardRef(function Link({ variant = 'body', ...props }, ref) {
-  return <StyledLink ref={ref} variant={variant} {...props} underline="none" />;
-}) as OverridableComponent<LinkTypeMap>;
-
-export type CustomNavLinkProps<
-  D extends React.ElementType = defaultComponent,
-  P = {}
-> = CustomLinkProps<D, P> & {
-  active?: boolean;
-  disabled?: boolean;
-};
-
-interface NavLinkTypeMap<D extends React.ElementType = defaultComponent, P = {}> {
-  props: CustomNavLinkProps<D, P>;
-  defaultComponent: D;
-}
-
-export type NavLinkProps<D extends React.ElementType = defaultComponent, P = {}> = OverrideProps<
-  NavLinkTypeMap<D, P>,
-  D
->;
-
-type StyledNavLinkProps = Pick<CustomNavLinkProps, 'active' | 'disabled'>;
-
-const StyledNavLink = styled(BaseLink, {
-  shouldForwardProp: prop => prop !== 'active' && prop !== 'disabled',
-})<StyledNavLinkProps>(({ active, disabled }) => {
-  const disabledStyles = disabled
-    ? {
-        transition: 'none',
-        opacity: 0.15,
-        cursor: 'not-allowed',
-        '&:hover': {},
-      }
-    : {};
-
-  const activeStyles =
-    !disabled && active
-      ? {
-          '&.MuiLink-root': {
-            color: colors.cyan,
-          },
-        }
-      : {};
-
-  return {
-    textDecoration: 'none',
+    ...(variant === 'inherit' && { textTransform: 'inherit', color: 'inherit' }),
+    textDecoration: 'underline',
+    textDecorationThickness: 2,
+    textUnderlineOffset: 4,
+    textDecorationColor: colors.cyan,
     '&:hover': {
-      color: colors.cyan,
+      opacity: 0.5,
     },
-    ...activeStyles,
-    ...disabledStyles,
   };
 });
 
-export const NavLink = React.forwardRef(function NavLink(
-  { onClick, active, disabled, variant = 'body', ...props },
+export interface TextLinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    Pick<MuiLinkProps, 'children' | 'classes' | 'sx' | 'variant'> {}
+
+const TextLink = React.forwardRef<HTMLAnchorElement, TextLinkProps>(function Link(
+  { variant = 'body', ...props },
   ref
 ) {
-  const handleClick = React.useCallback(
-    (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      if (disabled || active) {
-        e.preventDefault();
-        return;
-      }
+  return <StyledTextLink ref={ref} variant={variant} {...props} underline="none" />;
+});
 
-      if (onClick) {
-        onClick(e);
-      }
-    },
-    [onClick, disabled, active]
-  );
-
-  return (
-    <StyledNavLink
-      ref={ref}
-      variant={variant}
-      underline="none"
-      onClick={handleClick}
-      active={active}
-      disabled={disabled}
-      {...props}
-    />
-  );
-}) as OverridableComponent<NavLinkTypeMap>;
-
-export default Link;
+export default TextLink;
