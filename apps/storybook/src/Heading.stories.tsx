45f1d69e61fef6ae7ead2ba62e8b2869eb637ecb
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2022-11-24 16:47:05 +0000

    add Typography components (#12)

---
+++ apps/storybook/src/Heading.stories.tsx
@@ -0,0 +1,123 @@
+import Stack from '@mui/material/Stack';
+import { Meta, Story } from '@storybook/react';
+import { backgroundColors, Heading, Background, variantMapping } from 'uw-web-ui';
+import type { BackgroundProps, HeadingProps } from 'uw-web-ui';
+import BackgroundStack from './BackgroundStack';
+
+const variants = ['displayHeading', 'h1', 'h2', 'h3', 'h4'] as const;
+
+const colors = ['primary', 'secondary'] as const;
+
+export default {
+  title: 'Components/Heading',
+  component: Heading,
+  argTypes: {
+    forwardedRef: { table: { disable: true } },
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+    component: {
+      control: {
+        type: 'text',
+      },
+    },
+    variant: {
+      control: {
+        type: 'radio',
+        options: variants,
+      },
+    },
+    color: {
+      control: {
+        type: 'radio',
+        options: colors,
+      },
+    },
+    backgroundColor: {
+      control: {
+        type: 'radio',
+        options: backgroundColors,
+      },
+    },
+    letterSpacing: {
+      control: {
+        type: 'text',
+      },
+    },
+    textTransform: {
+      control: {
+        type: 'radio',
+        options: ['capitalize', 'uppercase', 'lowercase', 'none'],
+      },
+    },
+  },
+  args: {
+    backgroundColor: 'white',
+    children: 'hamburgefons',
+    variant: 'h2',
+    component: 'h2',
+    color: 'primary',
+    textTransform: 'capitalize',
+    gutterBottom: false,
+    paragraph: false,
+    noWrap: false,
+  },
+} as Meta;
+
+export const HeadingKitchenSinkStory: Story<
+  HeadingProps & { backgroundColor: BackgroundProps['backgroundColor'] }
+> = ({ backgroundColor, ...args }) => {
+  return (
+    <BackgroundStack>
+      <Stack spacing={4} direction="row">
+        {variants.map(v => (
+          <Stack key={v} spacing={2}>
+            {colors.map(c => (
+              <Heading
+                key={`${v}${c}`}
+                {...args}
+                variant={v}
+                component={variantMapping[v] as React.ElementType<any>}
+                color={c}
+              />
+            ))}
+          </Stack>
+        ))}
+      </Stack>
+    </BackgroundStack>
+  );
+};
+HeadingKitchenSinkStory.storyName = 'Kitchen Sink';
+HeadingKitchenSinkStory.argTypes = {
+  variant: { table: { disable: true } },
+  gutterBottom: { table: { disable: true } },
+  paragraph: { table: { disable: true } },
+  component: { table: { disable: true } },
+  classes: { table: { disable: true } },
+  sx: { table: { disable: true } },
+  align: { table: { disable: true } },
+  noWrap: { table: { disable: true } },
+  color: { table: { disable: true } },
+  backgroundColor: { table: { disable: true } },
+  textTransform: { table: { disable: true } },
+  letterSpacing: { table: { disable: true } },
+};
+HeadingKitchenSinkStory.args = {
+  color: 'primary',
+};
+
+export const HeadingCustomStory: Story<
+  HeadingProps & { backgroundColor: BackgroundProps['backgroundColor'] }
+> = ({ backgroundColor, ...args }) => {
+  return (
+    <Background
+      backgroundColor={backgroundColor}
+      sx={{ display: 'flex', justifyContent: 'center', padding: 4 }}
+    >
+      <Heading {...args} component="span" />
+    </Background>
+  );
+};
+HeadingCustomStory.storyName = 'Custom';
--- packages/uw-web-ui/src/Typography/index.ts
+++ packages/uw-web-ui/src/Typography/index.ts
@@ -1,3 +1,7 @@
 export { default, variantMapping } from './Typography';
 export type { TypographyProps } from './Typography';
 export { typographyThemeOverrides } from './Typography.theme';
+export { default as Text } from './Text';
+export type { TextProps } from './Text';
+export { default as Heading } from './Heading';
+export type { HeadingProps } from './Heading';
