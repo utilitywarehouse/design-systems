27b7c9c8f95ab4eb248c9644ec4bcef8ca9538f2
Rob Phoenix <rphoenix@uw.co.uk>
2022-11-24 16:42:58 +0000

    add Heading component

--- packages/uw-web-ui/src/Typography/index.ts
+++ packages/uw-web-ui/src/Typography/index.ts
@@ -3,3 +3,5 @@ export type { TypographyProps } from './Typography';
 export { typographyThemeOverrides } from './Typography.theme';
 export { default as Text } from './Text';
 export type { TextProps } from './Text';
+export { default as Heading } from './Heading';
+export type { HeadingProps } from './Heading';
