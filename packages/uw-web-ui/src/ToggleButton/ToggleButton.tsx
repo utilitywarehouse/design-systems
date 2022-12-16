f5b5f7a6da816726c26ba3438dfaa646f47d20ee
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2022-12-16 10:01:50 +0000

    Migrate Toggle Button (#11)
    Co-authored-by: Jord McCord <jmccord@uw.co.uk>

---
+++ packages/uw-web-ui/src/ToggleButton/ToggleButton.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import MuiToggleButton, {
+  ToggleButtonProps as MuiToggleButtonProps,
+} from '@mui/material/ToggleButton';
+import { colors, fonts, fontWeights } from '@utilitywarehouse/customer-ui-design-tokens';
+import { dataAttributes, isInverseBackgroundColor, px } from '../utils';
+import { useBackground } from '../Background';
+
+const StyledMuiToggleButton = styled(MuiToggleButton)(({ theme }) => {
+  const { inverse } = dataAttributes;
+  return {
+    border: 0,
+    padding: `${theme.spacing(0.5)} ${theme.spacing(3)}`,
+    borderRadius: px(32),
+    fontFamily: fonts.secondary,
+    fontWeight: fontWeights.secondary.semibold,
+    fontSize: 16,
+    lineHeight: 1.5,
+    color: colors.midnight,
+    '&:focus, &:active': {
+      backgroundColor: colors.cyan50,
+    },
+    '&:hover': {
+      backgroundColor: colors.cyan20,
+    },
+    '&:disabled': {
+      color: colors.codGray70,
+      border: 'none',
+    },
+    '&.Mui-selected, &.Mui-selected:hover': {
+      backgroundColor: colors.cyan,
+      '&:disabled': {
+        backgroundColor: colors.codGray20,
+      },
+    },
+    [`&[data-${inverse}=true]`]: {
+      color: colors.white,
+      '&:disabled': {
+        color: colors.codGray40,
+      },
+      '&.Mui-selected, &:hover, &:focus, &:active': {
+        color: colors.midnight,
+        '&:disabled': {
+          color: colors.codGray80,
+          backgroundColor: colors.codGray30,
+        },
+      },
+    },
+  };
+});
+
+export type ToggleButtonProps = Pick<
+  MuiToggleButtonProps,
+  | 'value'
+  | 'children'
+  | 'classes'
+  | 'className'
+  | 'disabled'
+  | 'fullWidth'
+  | 'onChange'
+  | 'onClick'
+  | 'selected'
+  | 'sx'
+>;
+
+const ToggleButton = React.forwardRef<HTMLButtonElement, ToggleButtonProps>(function ToggleButton(
+  { children, className, ...props },
+  ref
+) {
+  const { backgroundColor } = useBackground();
+  const inverse = isInverseBackgroundColor(backgroundColor);
+
+  const dataAttributeProps = {
+    [`data-${dataAttributes.inverse}`]: inverse,
+  };
+
+  return (
+    <StyledMuiToggleButton
+      ref={ref}
+      disableTouchRipple
+      disableRipple
+      disableFocusRipple
+      {...props}
+      className={className}
+      {...dataAttributeProps}
+    >
+      {children}
+    </StyledMuiToggleButton>
+  );
+});
+
+export default ToggleButton;
---
+++ packages/uw-web-ui/src/ToggleButton/index.ts
@@ -0,0 +1,4 @@
+export { default } from './ToggleButton';
+export type { ToggleButtonProps } from './ToggleButton';
+export { default as ToggleButtonGroup } from './ToggleButtonGroup';
+export type { ToggleButtonGroupProps } from './ToggleButtonGroup';
