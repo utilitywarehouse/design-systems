5a1e59c4f84bb663217042193d754172a1c385af
Rob Phoenix <rphoenix@uw.co.uk>
2023-02-03 10:01:41 +0000

    fix

--- packages/web-ui/src/Menu/MenuItem.tsx
+++ packages/web-ui/src/Menu/MenuItem.tsx
@@ -3,16 +3,16 @@ import {
   type MenuItemProps as MuiMenuItemProps,
   default as MuiMenuItem,
 } from '@mui/material/MenuItem';
-import Typography from '../Typography';
+import { Text } from '../Typography';
 
 export interface MenuItemProps extends MuiMenuItemProps {}
 
 const MenuItem = React.forwardRef<HTMLLIElement, MenuItemProps>(function MenuItem(props, ref) {
   return (
     <MuiMenuItem {...props} ref={ref}>
-      <Typography component="span" variant="body">
+      <Text component="span" variant="body">
         {props.children}
-      </Typography>
+      </Text>
     </MuiMenuItem>
   );
 });
