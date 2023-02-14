6f4a22c75b646760a3d5e519b207fd1c58a31d81
Rob Phoenix <rphoenix@uw.co.uk>
2023-02-14 14:33:59 +0000

    update Grid

--- packages/web-ui/src/Grid/Grid.tsx
+++ packages/web-ui/src/Grid/Grid.tsx
@@ -3,13 +3,13 @@ import MuiGrid, { GridProps as MuiGridProps } from '@mui/material/Grid';
 const DEFAULT_COLUMNS = { mobile: 4, tablet: 8, desktop: 12 };
 const DEFAULT_SPACING = { mobile: 2, tablet: 3, desktop: 3 };
 
-export interface GridProps extends MuiGridProps {}
+export type GridProps<C extends React.ElementType = 'div'> = MuiGridProps<C, { component?: C }>;
 
-export const Grid = function Grid({ columns = DEFAULT_COLUMNS, ...props }: GridProps) {
+function Grid<C extends React.ElementType>({ columns = DEFAULT_COLUMNS, ...props }: GridProps<C>) {
   if (props.container) {
     return <MuiGrid columns={columns} spacing={props.spacing || DEFAULT_SPACING} {...props} />;
   }
   return <MuiGrid columns={columns} {...props} />;
-};
+}
 
 export default Grid;
