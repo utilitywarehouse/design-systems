7d1b6155282623a769cda61847541efd43018260
Rob Phoenix <rphoenix@uw.co.uk>
2023-02-15 13:13:58 +0000

    use tsup

--- apps/bundle-workshop/pages/_app.tsx
+++ apps/bundle-workshop/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@utilitywarehouse/fontsource';
+import { ThemeProvider } from '@utilitywarehouse/web-ui';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
