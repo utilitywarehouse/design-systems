ac5fb0e66f143f362a373be9c7c63f612d27cd4f
Rob Phoenix <rphoenix@uw.co.uk>
2023-02-15 13:32:25 +0000

    tsup working with tree-shaking

---
+++ packages/web-ui/tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, Format } from 'tsup';
+
+export default defineConfig([
+  // actual exposed modules = 1 per component
+  {
+    // index files to allow named imports
+    entry: ['./src/**/*.ts?(x)'],
+    // treeshake: true,
+    sourcemap: true,
+    minify: true,
+    clean: true,
+    dts: true,
+    splitting: false,
+    outDir: 'dist',
+    format: ['cjs', 'esm'] as Array<Format>,
+    external: ['react', '*.stories.*'],
+    // index files must NOT be bundled!
+    // it acts as a map towards bundled components
+    // but never rebundles them
+    bundle: false,
+  },
+]);
