f865fafae6b5a339c7320aa6d76b7219b68c9861
Rob Phoenix <rphoenix@uw.co.uk>
2023-02-17 14:22:56 +0000

    minor updates

--- packages/web-ui/src/Box/Box.tsx
+++ packages/web-ui/src/Box/Box.tsx
@@ -5,6 +5,18 @@ import { OverridableComponent } from '@mui/material/OverridableComponent';
 import { forwardRef } from 'react';
 import { BoxTypeMap } from './Box.types';
 
+/**
+ * Box is a low-level primitive, which supports theme-aware styling props, and can
+ * be used for building any styled element.
+ *
+ * By default Box renders a div element, this can be customised using the
+ * `component` prop.
+ *
+ * Additionally to the system properties, Box supports pre-defined backgrounds,
+ * via the `background` prop that provide visual context to child components.
+ * This means that components will change foreground colour depending on the
+ * parent background colour.
+ */
 const Box = forwardRef(function Box({ background, sx, ...props }, ref) {
   const inverse = background ? isInverseBackgroundColor(background) : false;
   const dataAttributeProps = {
