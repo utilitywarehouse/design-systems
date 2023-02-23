fa32a236bb73c732559320a99abb277b8df85712
Rob Phoenix <rphoenix@uw.co.uk>
2023-02-23 10:56:36 +0000

    hide actions tab

--- packages/web-ui/.storybook/main.js
+++ packages/web-ui/.storybook/main.js
@@ -1,7 +1,16 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   stories: ['../docs/**/*.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-designs'],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false,
+      },
+    },
+    'storybook-addon-designs',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
