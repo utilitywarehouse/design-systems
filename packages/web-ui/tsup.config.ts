c0ee032b5dfbb46369034b90b60c68da5a479c73
Rob Phoenix <rphoenix@uw.co.uk>
2023-03-31 11:37:03 +0100

    feat: hold

--- packages/web-ui/tsup.config.ts
+++ packages/web-ui/tsup.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig, Format } from 'tsup';
+import path from 'path';
+import { defineConfig } from 'tsup';
 
+// actual exposed modules = 1 per component
 export default defineConfig([
-  // actual exposed modules = 1 per component
   {
     // index files to allow named imports
     entry: ['./src/**/*.ts?(x)', '!src/**/*.stories.*'],
-    tsconfig: './tsconfig.build.json',
-    // treeshake: true,
+    tsconfig: path.join(__dirname, './tsconfig.build.json'),
     sourcemap: true,
+    splitting: true,
+    clean: true, // clean up the dist folder
+    dts: true, // generate dts files
+    format: ['cjs', 'esm'], // generate cjs and esm files
     minify: true,
-    clean: true,
-    dts: true,
-    splitting: false,
-    outDir: 'dist',
-    format: ['cjs', 'esm'] as Array<Format>,
+    bundle: true,
+    skipNodeModulesBundle: true,
+    entryPoints: ['src/index.ts'],
     target: 'es2020',
-    external: ['react', '*.stories.*'],
-    // index files must NOT be bundled!
-    // it acts as a map towards bundled components
-    // but never rebundles them
-    bundle: false,
+    outDir: 'dist',
+    outExtension({ format }) {
+      return {
+        js: `.${format}.js`,
+      };
+    },
   },
 ]);
