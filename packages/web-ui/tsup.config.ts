5220380e937071ee39ec778af4802485386f7895
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2023-03-31 13:38:26 +0100

    tsup bundling updates (#47)
    Co-authored-by: Jordan McCord <jmccord@uw.co.uk>

--- packages/web-ui/tsup.config.ts
+++ packages/web-ui/tsup.config.ts
@@ -1,21 +1,17 @@
-import { defineConfig, Format } from 'tsup';
+import path from 'path';
+import { defineConfig } from 'tsup';
 
+// actual exposed modules = 1 per component
 export default defineConfig([
-  // actual exposed modules = 1 per component
   {
     // index files to allow named imports
     entry: ['./src/**/*.ts?(x)', '!src/**/*.stories.*'],
-    tsconfig: './tsconfig.build.json',
-    // treeshake: true,
-    sourcemap: true,
+    tsconfig: path.join(__dirname, './tsconfig.build.json'),
+    format: ['cjs', 'esm'], // generate cjs and esm files
+    target: 'es2020',
     minify: true,
     clean: true,
     dts: true,
-    splitting: false,
-    outDir: 'dist',
-    format: ['cjs', 'esm'] as Array<Format>,
-    target: 'es2020',
-    external: ['react', '*.stories.*'],
     // index files must NOT be bundled!
     // it acts as a map towards bundled components
     // but never rebundles them
