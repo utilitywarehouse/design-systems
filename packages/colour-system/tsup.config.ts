e10b15d2c1d40c5a740ebe3175fe59856f1b0c49
Rob Phoenix <rphoenix@uw.co.uk>
2023-04-28 13:29:37 +0100

    use tsup

--- packages/colour-system/tsup.config.ts
+++ packages/colour-system/tsup.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    entry: ['./src/index.ts'],
-    format: ['cjs', 'esm'],
-    target: 'es2020',
-    minify: true,
+    splitting: true,
+    sourcemap: true,
     clean: true,
     dts: true,
+    format: ['cjs', 'esm'],
+    minify: true,
     bundle: true,
+    skipNodeModulesBundle: true,
+    target: 'es2020',
+    outDir: 'dist',
+    entry: ['./src/**/*.ts'],
   },
 ]);
