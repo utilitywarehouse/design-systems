ea2a859854d121ec9af0c1ce0b42b1a3e90d96b5
Rob Phoenix <rphoenix@uw.co.uk>
2023-05-04 12:36:43 +0100

    add common colours

---
+++ packages/colour-system/src/common/index.ts
@@ -0,0 +1,5 @@
+// Hey, don't edit this file directly, it's been magically generated.
+/** White can be utilised in any theme as a background or foreground color.  */
+export const midnight = '#1e0a46';
+/** White can be utilised in any theme as a background, border, or foreground color.  */
+export const white = '#ffffff';
--- packages/colour-system/src/index.ts
+++ packages/colour-system/src/index.ts
@@ -1,3 +1,4 @@
 // Hey, don't edit this file directly, it's been magically generated.
 export * as colors from './light';
 export * as colorsDark from './dark';
+export * as colorsCommon from './common';
