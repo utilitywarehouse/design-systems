617faf1190ad602d5d69ea365484a77535da7b80
Rob Phoenix <rphoenix@uw.co.uk>
2023-05-17 10:37:13 +0100

    update legacy typography theme

--- packages/web-ui/src/Typography/Typography.theme.ts
+++ packages/web-ui/src/Typography/Typography.theme.ts
@@ -46,95 +46,85 @@ const legacyHeadingStyles = {
   },
 };
 
-const body = {
-  ...baseTextStyles,
-  fontSize: pxToRem(16),
-  lineHeight: 1.5,
-};
-const subtitle = {
-  ...baseTextStyles,
-  fontSize: pxToRem(18),
-  lineHeight: 1.5,
-  [mediaQueries.desktop]: {
-    fontSize: pxToRem(20),
-  },
-};
-const legalNote = {
-  ...baseTextStyles,
-  fontSize: pxToRem(14),
-  lineHeight: 1.5,
-};
-const caption = {
-  ...baseTextStyles,
-  fontSize: pxToRem(12),
-  lineHeight: 2,
-};
-const displayHeading = {
-  ...baseHeadingStyles,
-  fontSize: pxToRem(42),
-  lineHeight: 1,
-  [mediaQueries.desktop]: {
-    fontSize: pxToRem(64),
-  },
-};
-const h1 = {
-  ...baseHeadingStyles,
-  fontSize: pxToRem(32),
-  lineHeight: 1.2,
-  [mediaQueries.desktop]: {
-    fontSize: pxToRem(42),
-  },
-};
-const h2 = {
-  ...baseHeadingStyles,
-  fontSize: pxToRem(28),
-  lineHeight: 1.5,
-  [mediaQueries.desktop]: {
-    fontSize: pxToRem(32),
-    lineHeight: 1.2,
-  },
-};
-const h3 = {
-  ...baseHeadingStyles,
-  fontSize: pxToRem(22),
-  lineHeight: 1.5,
-  [mediaQueries.desktop]: {
-    fontSize: pxToRem(24),
-  },
-};
-const h4 = {
-  ...baseHeadingStyles,
-  fontSize: pxToRem(18),
-  lineHeight: 1.5,
-  [mediaQueries.desktop]: {
-    fontSize: pxToRem(20),
-  },
-};
-
 // we do this so that consumers can access this separately to the components.
 // ie sx={{ ...theme.typography.body }}
 export const baseTypographyTheme = {
-  body,
-  subtitle,
-  legalNote,
-  caption,
-  displayHeading,
-  h1,
-  h2,
-  h3,
-  h4,
+  body: {
+    ...baseTextStyles,
+    fontSize: pxToRem(16),
+    lineHeight: 1.5,
+  },
+  subtitle: {
+    ...baseTextStyles,
+    fontSize: pxToRem(18),
+    lineHeight: 1.5,
+    [mediaQueries.desktop]: {
+      fontSize: pxToRem(20),
+    },
+  },
+  legalNote: {
+    ...baseTextStyles,
+    fontSize: pxToRem(14),
+    lineHeight: 1.5,
+  },
+  caption: {
+    ...baseTextStyles,
+    fontSize: pxToRem(12),
+    lineHeight: 2,
+  },
+  displayHeading: {
+    ...baseHeadingStyles,
+    fontSize: pxToRem(42),
+    lineHeight: 1,
+    [mediaQueries.desktop]: {
+      fontSize: pxToRem(64),
+    },
+  },
+  h1: {
+    ...baseHeadingStyles,
+    fontSize: pxToRem(32),
+    lineHeight: 1.2,
+    [mediaQueries.desktop]: {
+      fontSize: pxToRem(42),
+    },
+  },
+  h2: {
+    ...baseHeadingStyles,
+    fontSize: pxToRem(28),
+    lineHeight: 1.5,
+    [mediaQueries.desktop]: {
+      fontSize: pxToRem(32),
+      lineHeight: 1.2,
+    },
+  },
+  h3: {
+    ...baseHeadingStyles,
+    fontSize: pxToRem(22),
+    lineHeight: 1.5,
+    [mediaQueries.desktop]: {
+      fontSize: pxToRem(24),
+    },
+  },
+  h4: {
+    ...baseHeadingStyles,
+    fontSize: pxToRem(18),
+    lineHeight: 1.5,
+    [mediaQueries.desktop]: {
+      fontSize: pxToRem(20),
+    },
+  },
 };
 
 // and then this is for the components styles.
 // We'll be able to unify this in v1, when we no longer support the legacy color styles.
-export const typographyThemeOverrides = {
-  body: { ...body, ...legacyTextStyles },
-  subtitle: { ...subtitle, ...legacyTextStyles },
-  legalNote: { ...legalNote, ...legacyTextStyles },
-  caption: { ...caption, ...legacyTextStyles },
-  displayHeading: { ...displayHeading, ...legacyHeadingStyles },
-  h1: { ...h1, ...legacyHeadingStyles },
-  h2: { ...h2, ...legacyHeadingStyles },
-  h3: { ...h3, ...legacyHeadingStyles },
-  h4: { ...h4, ...legacyHeadingStyles },
+export const legacyTypographyThemeOverrides = {
+  body: { ...legacyTextStyles },
+  subtitle: { ...legacyTextStyles },
+  legalNote: { ...legacyTextStyles },
+  caption: { ...legacyTextStyles },
+  displayHeading: { ...legacyHeadingStyles },
+  h1: { ...legacyHeadingStyles },
+  h2: { ...legacyHeadingStyles },
+  h3: { ...legacyHeadingStyles },
+  h4: { ...legacyHeadingStyles },
 };
