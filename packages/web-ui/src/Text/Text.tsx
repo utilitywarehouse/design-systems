682335c611335f7a2439533834808b078a3783ae
Rob Phoenix <rphoenix@uw.co.uk>
2023-05-25 15:48:37 +0100

    replace Text variants with sizes

--- packages/web-ui/src/Text/Text.tsx
+++ packages/web-ui/src/Text/Text.tsx
@@ -1,70 +1,73 @@
-import { forwardRef } from 'react';
-import MuiTypography, { TypographyProps as MuiTypographyProps } from '@mui/material/Typography';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
-import type { OverrideProps } from '@mui/material/OverridableComponent';
-import type { SystemProps } from '../types';
-import { fontWeights } from '../tokens';
+import { TypographyProps as MuiTypographyProps } from '@mui/material/Typography';
+import { fonts, fontWeights } from '../tokens';
+import { Box, BoxProps } from '../Box';
+import { colorsCommon } from '@utilitywarehouse/colour-system';
+import { pxToRem } from '../utils';
 
-export type DefaultTextComponent = 'span';
-
-export const textVariantMapping: Record<string, string> = {
-  subtitle: 'p',
-  body: 'p',
-  legalNote: 'p',
-  caption: 'span',
-};
-
-export interface CustomTextProps {
-  /**
-   * Applies the theme typography styles.
-   * @default body
-   */
-  variant: 'subtitle' | 'body' | 'legalNote' | 'caption';
-  /**
-   * The component used for the root node. Either a string to use a HTML element or a component.
-   */
-  component: React.ElementType;
-  /**
-   * Set the text color. It is recommended to use the colours from the `@utilitywarehouse/colour-system` package.
-   * @default colorsCommon.brandMidnight
-   */
-  color?: string;
-  /**
-   * Set the font-weight to semibold.
-   * @default false
-   */
-  bold?: boolean;
-  /**
-   * Set the text-transform property on the component.
-   */
-  textTransform?: MuiTypographyProps['textTransform'];
-}
-
-export interface TextTypeMap<D extends React.ElementType = DefaultTextComponent, P = {}> {
-  props: CustomTextProps & Omit<MuiTypographyProps<D, P>, 'variant' | SystemProps>;
-  defaultComponent: D;
-}
-
-export type TextProps<D extends React.ElementType = DefaultTextComponent, P = {}> = OverrideProps<
-  TextTypeMap<D, P>,
-  D
->;
+export type TextProps = Pick<BoxProps, 'sx' | 'component' | 'children'> &
+  Pick<MuiTypographyProps, 'textTransform' | 'align' | 'noWrap'> & {
+    /**
+     * Applies the typography size styles.
+     * @default md
+     */
+    size?: 'lg' | 'md' | 'sm' | 'xs';
+    /**
+     * Set the text color. It is recommended to use the colours from the `@utilitywarehouse/colour-system` package.
+     * @default colorsCommon.brandMidnight
+     */
+    color?: string;
+    /**
+     * Set the font-weight to semibold.
+     * @default false
+     */
+    bold?: boolean;
+  };
 
 /**
  * Text renders the secondary UW font, Work Sans, to be used for body copy.
  */
-export const Text = forwardRef(function Text(
-  { color, variant = 'body', bold = false, ...props },
-  ref
-) {
+export const Text = ({
+  color = colorsCommon.brandMidnight,
+  bold = false,
+  component = 'span',
+  size = 'md',
+  align,
+  noWrap,
+  sx,
+  ...props
+}: TextProps) => {
+  const fontFamily = fonts.secondary;
+  const fontWeight = bold ? fontWeights.secondary.semibold : undefined;
+  const fontSizes = {
+    xs: pxToRem(12),
+    sm: pxToRem(14),
+    md: pxToRem(16),
+    lg: {
+      mobile: pxToRem(18),
+      desktop: pxToRem(20),
+    },
+  };
+
+  const noWrapStyles = {
+    ...sx,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  };
+
   return (
-    <MuiTypography
-      ref={ref}
-      variantMapping={textVariantMapping}
-      variant={variant}
+    <Box
       color={color}
-      fontWeight={bold ? fontWeights.secondary.semibold : undefined}
+      component={component}
+      fontFamily={fontFamily}
+      fontSize={fontSizes[size]}
+      fontWeight={fontWeight}
+      lineHeight={size === 'xs' ? 2 : 1.5}
+      textAlign={align}
+      sx={noWrap ? noWrapStyles : sx}
       {...props}
     />
   );
-}) as OverridableComponent<TextTypeMap>;
+};
+
+Text.displayName = 'Text';
