f8d7c2c48b2b110d38c1f33d42f8707907d759a2
Rob Phoenix <rphoenix@uw.co.uk>
2023-06-26 12:10:55 +0100

    fix after rebase

--- packages/web-ui/src/utils/index.ts
+++ packages/web-ui/src/utils/index.ts
@@ -2,4 +2,4 @@ export { dataAttributes } from './data-attributes';
 export { htmlFontSize, pxToRem, isHeadingVariant } from './typography';
 export { mediaQueries } from './media-queries';
 export { isInverseBackgroundColor } from './color';
-export { px, spacing } from './utils';
+export { px, spacing, classNamePrefix } from './utils';
--- packages/web-ui/src/utils/utils.ts
+++ packages/web-ui/src/utils/utils.ts
@@ -3,3 +3,5 @@ import { spacingBase } from '../tokens';
 export const px = (value: string | number): string => `${value}px`;
 
 export const spacing = (multiplier: number) => multiplier * spacingBase;
+
+export const classNamePrefix = 'web-ui-';
