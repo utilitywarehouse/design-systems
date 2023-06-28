6af5f9d1eb11aa4b08e23e0287a33ac42367fd35
Rob Phoenix <rphoenix@uw.co.uk>
2023-06-28 15:58:17 +0100

    update Stack docs

--- packages/web-ui/src/Stack/Stack.tsx
+++ packages/web-ui/src/Stack/Stack.tsx
@@ -15,6 +15,14 @@ export type StackProps<D extends React.ElementType = DefaultStackComponent, P = 
   D
 >;
 
+/**
+ * Stack is a layout primitive, for managing layout of immediate children along the
+ * vertical or horizontal axis with optional spacing and/or dividers between each
+ * child.
+ *
+ * This component is directly based on the one from the '@mui/material' package, please
+ * [check their site for further documentation](https://mui.com/material-ui/react-stack/).
+ */
 export const Stack = forwardRef(function Stack(props, ref) {
   return <MuiStack ref={ref} {...props} />;
 }) as OverridableComponent<StackTypeMap>;
