5075ec736f1e29aa82ec0d8e2959318faec661f9
Rob Phoenix <rphoenix@uw.co.uk>
2023-07-05 14:16:04 +0100

    update docs

--- packages/web-ui/src/Menu/Menu.tsx
+++ packages/web-ui/src/Menu/Menu.tsx
@@ -1,17 +1,21 @@
 import { forwardRef } from 'react';
 import MuiMenu, { type MenuProps as MuiMenuProps } from '@mui/material/Menu';
-import { Box } from '../Box';
-import { colorsCommon } from '@utilitywarehouse/colour-system';
 
 export interface MenuProps extends Omit<MuiMenuProps, 'ref'> {}
 
+/**
+ * A menu displays a list of choices on a temporary surface. It appears when
+ * the user interacts with a button, or other control.
+ *
+ * Please refer to [MUI](https://mui.com/material-ui/react-menu/) for more extensive documentation.
+ */
 export const Menu = forwardRef<HTMLDivElement, MenuProps>(function Menu(
   { children, ...props },
   ref
 ) {
   return (
     <MuiMenu ref={ref} {...props}>
-      <Box bgcolor={colorsCommon.brandWhite}>{children}</Box>
+      {children}
     </MuiMenu>
   );
 });
