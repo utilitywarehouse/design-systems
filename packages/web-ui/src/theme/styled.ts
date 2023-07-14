72148d0ef4d0284c211f9e99513fa3f5dc36e9cb
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2023-07-14 11:58:03 +0100

    enable component selectors API (#122)

--- packages/web-ui/src/theme/styled.ts
+++ packages/web-ui/src/theme/styled.ts
@@ -1,6 +1,8 @@
 import { createStyled } from '@mui/system';
 import { theme as defaultTheme, Theme } from './theme';
 
+// NOTE: this exists for external consumers, internally we will use the `styled` function from `@mui/material`.
+
 const styled = createStyled<Theme>({ defaultTheme });
 
 export default styled;
