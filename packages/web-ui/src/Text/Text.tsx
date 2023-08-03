090bdfa14d16e7c0cc3e51f12c1df3dbbb27cf75
Rob Phoenix <rphoenix@uw.co.uk>
2023-08-03 11:21:03 +0100

    refactor Text component

--- packages/web-ui/src/Text/Text.tsx
+++ packages/web-ui/src/Text/Text.tsx
@@ -1,9 +1,12 @@
-import { TypographyProps as MuiTypographyProps } from '@mui/material/Typography';
 import { fonts, fontWeights } from '../tokens';
-import { Box, BoxProps, useBackground } from '../Box';
+import { useBackground } from '../Box';
 import { colorsCommon } from '@utilitywarehouse/colour-system';
 import { pxToRem } from '../utils';
 import { SxProps } from '../types';
+import { ElementType, PropsWithChildren } from 'react';
+import styled, { FunctionInterpolation } from '@emotion/styled';
+import { unstable_styleFunctionSx as styleFunctionSx } from '@mui/system';
+import { useMediaQuery } from '../hooks';
 
 export type TextProps = {
   /**
@@ -14,7 +17,10 @@ export type TextProps = {
   /**
    * Sets the text color.
    * It is recommended to use the colours from the `@utilitywarehouse/colour-system` package.
-   * @default colorsCommon.brandMidnight
+   *
+   * The default color is `colors.brandMidnight` unless within a `Box`
+   * component with the background prop set to a darker brand background, then
+   * it will be `colors.brandWhite`.
    */
   color?: string;
   /**
@@ -22,58 +28,68 @@ export type TextProps = {
    * @default false
    */
   bold?: boolean;
-} & SxProps &
-  Pick<BoxProps, 'component' | 'children'> &
-  Pick<MuiTypographyProps, 'textTransform' | 'align' | 'noWrap'>;
+  /**
+   * Sets the HTML component that is rendered.
+   * @default p
+   */
+  component?: ElementType<any> | undefined;
+  /**
+   * Set the text-align on the component.
+   * @default 'inherit'
+   */
+  align?: 'right' | 'left' | 'inherit' | 'center' | 'justify' | undefined;
+  /**
+   * If true, the text will not wrap, but instead will truncate with a text overflow ellipsis.
+   * Note that text overflow can only happen with block or inline-block level elements (the element needs to have a width in order to overflow).
+   */
+  noWrap?: boolean | undefined;
+  /**
+   * Set the text-transform on the component.
+   * @default 'none'
+   */
+  textTransform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase' | undefined;
+} & SxProps;
+
+const displayName = 'Text';
+
+const StyledText = styled('p', {
+  label: displayName,
+  shouldForwardProp: prop =>
+    prop !== 'color' && prop !== 'as' && prop !== 'textTransform' && prop !== 'variant',
+})<TextProps>(
+  styleFunctionSx as FunctionInterpolation<TextProps>,
+  ({ bold, noWrap, variant, color, align, textTransform }) => {
+    const isDesktop = useMediaQuery(theme => theme.breakpoints.up('desktop'));
+    const fontSizes: { [key: string]: any } = {
+      caption: pxToRem(12),
+      legalNote: pxToRem(14),
+      body: pxToRem(16),
+      subtitle: pxToRem(isDesktop ? 20 : 18),
+    };
+    const { isBrandBackground } = useBackground();
+    const defaultColor = isBrandBackground ? colorsCommon.brandWhite : colorsCommon.brandMidnight;
+    return {
+      fontFamily: fonts.secondary,
+      fontSize: variant ? fontSizes[variant] : fontSizes.body,
+      fontWeight: fontWeights.secondary[bold ? 'semibold' : 'regular'],
+      color: color || defaultColor,
+      lineHeight: variant === 'caption' ? 2 : 1.5,
+      textTransform,
+      textAlign: align,
+      ...(noWrap && {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+      }),
+    };
+  }
+);
 
 /**
  * Text renders the secondary UW font, Work Sans, to be used for body copy.
  */
-export const Text = ({
-  color,
-  bold = false,
-  component = 'p',
-  variant = 'body',
-  align,
-  noWrap,
-  sx,
-  ...props
-}: TextProps) => {
-  const fontFamily = fonts.secondary;
-  const fontWeight = bold ? fontWeights.secondary.semibold : undefined;
-  const fontSizes = {
-    caption: pxToRem(12),
-    legalNote: pxToRem(14),
-    body: pxToRem(16),
-    subtitle: {
-      mobile: pxToRem(18),
-      desktop: pxToRem(20),
-    },
-  };
-
-  const noWrapStyles = {
-    ...sx,
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-  };
-
-  const { isBrandBackground } = useBackground();
-  const defaultColor = isBrandBackground ? colorsCommon.brandWhite : colorsCommon.brandMidnight;
-
-  return (
-    <Box
-      color={color || defaultColor}
-      component={component}
-      fontFamily={fontFamily}
-      fontSize={fontSizes[variant]}
-      fontWeight={fontWeight}
-      lineHeight={variant === 'caption' ? 2 : 1.5}
-      textAlign={align}
-      sx={noWrap ? noWrapStyles : sx}
-      {...props}
-    />
-  );
+export const Text = ({ component = 'p', ...props }: PropsWithChildren<TextProps>) => {
+  return <StyledText as={component} {...props} />;
 };
 
-Text.displayName = 'Text';
+Text.displayName = displayName;
