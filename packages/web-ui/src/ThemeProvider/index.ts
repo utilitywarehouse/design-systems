c778efad059bfb6c7b20734d5da89397e2cd284b
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2023-08-03 09:00:51 +0100

    use mui/system themeprovider (#136)

--- packages/web-ui/src/ThemeProvider/index.ts
+++ packages/web-ui/src/ThemeProvider/index.ts
@@ -1,2 +1,3 @@
 export { ThemeProvider } from './ThemeProvider';
 export type { ThemeProviderProps } from './ThemeProvider';
+export { useTheme } from '@mui/system';
--- packages/web-ui/src/hooks/use-media-query.ts
+++ packages/web-ui/src/hooks/use-media-query.ts
@@ -1,5 +1,6 @@
-import { Theme, useTheme } from '../theme';
+import { Theme } from '../theme';
 import { useMediaQuery as useMuiMediaQuery } from '@mui/material';
+import { useTheme } from '../ThemeProvider';
 
 // https://github.com/mui/material-ui/issues/16859#issuecomment-832560383
 export const useMediaQuery = (queryInput: string | ((theme: Theme) => string)) => {
