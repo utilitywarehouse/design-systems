a2770b27bdb72713f0b8b25468aa9b7c55accc75
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2023-08-03 13:42:21 +0100

    update Text component (#140)

--- packages/web-ui/src/Text/Text.tsx
+++ packages/web-ui/src/Text/Text.tsx
@@ -1,9 +1,21 @@
-import { TypographyProps as MuiTypographyProps } from '@mui/material/Typography';
 import { fonts, fontWeights } from '../tokens';
-import { Box, BoxProps, useBackground } from '../Box';
+import { useBackground } from '../Box';
 import { colorsCommon } from '@utilitywarehouse/colour-system';
 import { pxToRem } from '../utils';
 import { SxProps } from '../types';
+import { ElementType, PropsWithChildren } from 'react';
+import {
+  palette,
+  PaletteProps,
+  typography,
+  TypographyProps,
+  unstable_styleFunctionSx as styleFunctionSx,
+} from '@mui/system';
+import styled, { FunctionInterpolation } from '@emotion/styled';
+import isPropValid from '@emotion/is-prop-valid';
+import { ResponsiveStyleValue } from '@mui/system/styleFunctionSx';
+
+const displayName = 'Text';
 
 export type TextProps = {
   /**
@@ -14,6 +26,11 @@ export type TextProps = {
   /**
    * Sets the text color.
    * It is recommended to use the colours from the `@utilitywarehouse/colour-system` package.
+   *
+   * The default color is `colorsCommon.brandMidnight` unless within a `Box`
+   * component with the background prop set to a darker brand background, then
+   * it will be `colorsCommon.brandWhite`.
+   *
    * @default colorsCommon.brandMidnight
    */
   color?: string;
@@ -22,25 +39,64 @@ export type TextProps = {
    * @default false
    */
   bold?: boolean;
-} & SxProps &
-  Pick<BoxProps, 'component' | 'children'> &
-  Pick<MuiTypographyProps, 'textTransform' | 'align' | 'noWrap'>;
+  /**
+   * Sets the HTML component that is rendered.
+   * @default p
+   */
+  component?: ElementType<any> | undefined;
+  /**
+   * If true, the text will not wrap, but instead will truncate with a text overflow ellipsis.
+   * Note that text overflow can only happen with block or inline-block level elements (the element needs to have a width in order to overflow).
+   */
+  noWrap?: boolean | undefined;
+  /**
+   * Set the text-align on the component.
+   * @default 'inherit'
+   */
+  align?: ResponsiveStyleValue<'right' | 'left' | 'inherit' | 'center' | 'justify' | undefined>;
+  /**
+   * Set the text-transform on the component.
+   * @default 'none'
+   */
+  textTransform?: ResponsiveStyleValue<
+    'none' | 'capitalize' | 'uppercase' | 'lowercase' | undefined
+  >;
+} & SxProps;
+
+const StyledText = styled('p', {
+  label: displayName,
+  shouldForwardProp: prop => isPropValid(prop) && prop !== 'color' && prop !== 'fontSize',
+})<TextProps & PaletteProps & TypographyProps>(
+  palette,
+  typography,
+  styleFunctionSx as FunctionInterpolation<TextProps>,
+  ({ bold, noWrap, variant, color }) => {
+    const { isBrandBackground } = useBackground();
+    return {
+      fontFamily: fonts.secondary,
+      fontWeight: fontWeights.secondary[bold ? 'semibold' : 'regular'],
+      lineHeight: variant === 'caption' ? 2 : 1.5,
+      color: color || colorsCommon.brandMidnight,
+      ...(isBrandBackground && { color: color || colorsCommon.brandWhite }),
+      ...(noWrap && {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+      }),
+    };
+  }
+);
 
 /**
  * Text renders the secondary UW font, Work Sans, to be used for body copy.
  */
 export const Text = ({
-  color,
-  bold = false,
   component = 'p',
   variant = 'body',
   align,
-  noWrap,
-  sx,
+  textTransform,
   ...props
-}: TextProps) => {
-  const fontFamily = fonts.secondary;
-  const fontWeight = bold ? fontWeights.secondary.semibold : undefined;
+}: PropsWithChildren<TextProps>) => {
   const fontSizes = {
     caption: pxToRem(12),
     legalNote: pxToRem(14),
@@ -50,30 +106,14 @@ export const Text = ({
       desktop: pxToRem(20),
     },
   };
-
-  const noWrapStyles = {
-    ...sx,
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
+  const combinedProps = {
+    as: component,
+    textTransform,
+    textAlign: align,
+    fontSize: fontSizes[variant],
+    ...props,
   };
-
-  const { isBrandBackground } = useBackground();
-  const defaultColor = isBrandBackground ? colorsCommon.brandWhite : colorsCommon.brandMidnight;
-
-  return (
-    <Box
-      color={color || defaultColor}
-      component={component}
-      fontFamily={fontFamily}
-      fontSize={fontSizes[variant]}
-      fontWeight={fontWeight}
-      lineHeight={variant === 'caption' ? 2 : 1.5}
-      textAlign={align}
-      sx={noWrap ? noWrapStyles : sx}
-      {...props}
-    />
-  );
+  return <StyledText {...combinedProps} />;
 };
 
-Text.displayName = 'Text';
+Text.displayName = displayName;
