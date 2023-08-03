a059540b9ce4347a3e5688fa8f3ac8a3f704fb80
Rob Phoenix <rphoenix@uw.co.uk>
2023-08-03 12:12:06 +0100

    refactor Text component

--- packages/web-ui/src/Text/Text.tsx
+++ packages/web-ui/src/Text/Text.tsx
@@ -1,10 +1,20 @@
-import { TypographyProps as MuiTypographyProps } from '@mui/material/Typography';
 import { fonts, fontWeights } from '../tokens';
-import { Box, useBackground } from '../Box';
+import { useBackground } from '../Box';
 import { colorsCommon } from '@utilitywarehouse/colour-system';
 import { pxToRem } from '../utils';
 import { SxProps } from '../types';
 import { ElementType, PropsWithChildren } from 'react';
+import {
+  palette,
+  PaletteProps,
+  typography,
+  TypographyProps,
+  unstable_styleFunctionSx as styleFunctionSx,
+} from '@mui/system';
+import styled, { FunctionInterpolation } from '@emotion/styled';
+import { useMediaQuery } from '../hooks';
+import isPropValid from '@emotion/is-prop-valid';
+import { ResponsiveStyleValue } from '@mui/system/styleFunctionSx';
 
 export type TextProps = {
   /**
@@ -32,55 +42,55 @@ export type TextProps = {
    * @default p
    */
   component?: ElementType<any> | undefined;
-} & SxProps &
-  Pick<MuiTypographyProps, 'textTransform' | 'align' | 'noWrap'>;
+  /**
+   * If true, the text will not wrap, but instead will truncate with a text overflow ellipsis.
+   * Note that text overflow can only happen with block or inline-block level elements (the element needs to have a width in order to overflow).
+   */
+  noWrap?: boolean | undefined;
+  /**
+   * Set the text-align on the component.
+   * @default 'inherit'
+   */
+  align?: ResponsiveStyleValue<'right' | 'left' | 'inherit' | 'center' | 'justify' | undefined>;
+} & SxProps;
+
+const StyledText = styled('p', {
+  label: 'Text',
+  shouldForwardProp: prop => isPropValid(prop) && prop !== 'color',
+})<TextProps & PaletteProps & TypographyProps>(
+  palette,
+  typography,
+  styleFunctionSx as FunctionInterpolation<TextProps>,
+  ({ bold, noWrap, variant, color }) => {
+    const isDesktop = useMediaQuery(theme => theme.breakpoints.up('desktop'));
+    const fontSizes: { [key: string]: any } = {
+      caption: pxToRem(12),
+      legalNote: pxToRem(14),
+      body: pxToRem(16),
+      subtitle: pxToRem(isDesktop ? 20 : 18),
+    };
+    const { isBrandBackground } = useBackground();
+    const defaultColor = isBrandBackground ? colorsCommon.brandWhite : colorsCommon.brandMidnight;
+    return {
+      fontFamily: fonts.secondary,
+      fontSize: variant ? fontSizes[variant] : fontSizes.body,
+      fontWeight: fontWeights.secondary[bold ? 'semibold' : 'regular'],
+      color: color || defaultColor,
+      lineHeight: variant === 'caption' ? 2 : 1.5,
+      ...(noWrap && {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+      }),
+    };
+  }
+);
 
 /**
  * Text renders the secondary UW font, Work Sans, to be used for body copy.
  */
-export const Text = ({
-  color,
-  bold = false,
-  component = 'p',
-  variant = 'body',
-  align,
-  noWrap,
-  sx,
-  ...props
-}: PropsWithChildren<TextProps>) => {
-  const fontSizes = {
-    caption: pxToRem(12),
-    legalNote: pxToRem(14),
-    body: pxToRem(16),
-    subtitle: {
-      mobile: pxToRem(18),
-      desktop: pxToRem(20),
-    },
-  };
-
-  const { isBrandBackground } = useBackground();
-  const defaultColor = isBrandBackground ? colorsCommon.brandWhite : colorsCommon.brandMidnight;
-
-  return (
-    <Box
-      color={color || defaultColor}
-      component={component}
-      fontFamily={fonts.secondary}
-      fontSize={fontSizes[variant]}
-      fontWeight={fontWeights.secondary[bold ? 'semibold' : 'regular']}
-      lineHeight={variant === 'caption' ? 2 : 1.5}
-      textAlign={align}
-      sx={{
-        ...(noWrap && {
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-          whiteSpace: 'nowrap',
-        }),
-        ...sx,
-      }}
-      {...props}
-    />
-  );
+export const Text = ({ component = 'p', align, sx, ...props }: PropsWithChildren<TextProps>) => {
+  return <StyledText as={component} {...props} sx={{ textAlign: align, ...sx }} />;
 };
 
 Text.displayName = 'Text';
