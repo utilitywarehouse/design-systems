4d5f18d52f9bbce448913aa9dd2366f5d44a7fea
Rob Phoenix <rphoenix@uw.co.uk>
2023-08-29 13:49:32 +0100

    outline Typography API

--- packages/web-ui/src/Text/Text.tsx
+++ packages/web-ui/src/Text/Text.tsx
@@ -1,21 +1,59 @@
 import { fonts, fontWeights } from '../tokens';
 import { useBackground } from '../Box';
 import { colorsCommon } from '@utilitywarehouse/colour-system';
-import { pxToRem } from '../utils';
+import { pxToRem, globalPrefix } from '../utils';
 import { PropsWithStyleOverrides } from '../types';
-import { ElementType, PropsWithChildren } from 'react';
-import {
-  palette,
-  PaletteProps,
-  typography,
-  TypographyProps,
-  unstable_styleFunctionSx as styleFunctionSx,
-} from '@mui/system';
-import styled, { FunctionInterpolation } from '@emotion/styled';
-import isPropValid from '@emotion/is-prop-valid';
-import { ResponsiveStyleValue } from '@mui/system/styleFunctionSx';
+import { PropsWithChildren } from 'react';
+import { BoxProps as MuiBoxProps, createBox, ResponsiveStyleValue } from '@mui/system';
+
+import { theme, type Theme } from '../theme';
 
 const displayName = 'Text';
+
+const BaseBox = createBox<Theme>({
+  defaultTheme: theme,
+  defaultClassName: `${globalPrefix}-Typography`,
+});
+
+export interface TypographyProps
+  extends Pick<
+    MuiBoxProps,
+    | 'component'
+    | 'fontSize'
+    | 'lineHeight'
+    | 'letterSpacing'
+    | 'textTransform'
+    | 'textAlign'
+    | 'color'
+  > {
+  fontFamily: 'primary' | 'secondary';
+  fontWeight?: 'regular' | 'semibold';
+  noWrap?: boolean | undefined;
+}
+
+const Typography = ({
+  fontFamily,
+  fontWeight = 'regular',
+  sx,
+  noWrap,
+  ...props
+}: PropsWithChildren<PropsWithStyleOverrides<TypographyProps>>) => {
+  return (
+    <BaseBox
+      fontFamily={fonts[fontFamily]}
+      fontWeight={fontWeights.secondary[fontWeight]}
+      {...props}
+      sx={{
+        ...(noWrap && {
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap',
+        }),
+        ...sx,
+      }}
+    />
+  );
+};
 
 export type TextProps = {
   /**
@@ -43,7 +81,7 @@ export type TextProps = {
    * Sets the HTML component that is rendered.
    * @default p
    */
-  component?: ElementType<any> | undefined;
+  component?: MuiBoxProps['component'];
   /**
    * If true, the text will not wrap, but instead will truncate with a text overflow ellipsis.
    * Note that text overflow can only happen with block or inline-block level elements (the element needs to have a width in order to overflow).
@@ -63,30 +101,6 @@ export type TextProps = {
   >;
 };
 
-const StyledText = styled('p', {
-  label: displayName,
-  shouldForwardProp: prop => isPropValid(prop) && prop !== 'color' && prop !== 'fontSize',
-})<TextProps & PaletteProps & TypographyProps>(
-  palette,
-  typography,
-  ({ bold, noWrap, variant, color }) => {
-    const { isBrandBackground } = useBackground();
-    return {
-      fontFamily: fonts.secondary,
-      fontWeight: fontWeights.secondary[bold ? 'semibold' : 'regular'],
-      lineHeight: variant === 'caption' ? 2 : 1.5,
-      color: color || colorsCommon.brandMidnight,
-      ...(isBrandBackground && { color: color || colorsCommon.brandWhite }),
-      ...(noWrap && {
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-      }),
-    };
-  },
-  styleFunctionSx as FunctionInterpolation<TextProps>
-);
-
 /**
  * Text renders the secondary UW font, Work Sans, to be used for body copy.
  */
@@ -94,7 +108,8 @@ export const Text = ({
   component = 'p',
   variant = 'body',
   align,
-  textTransform,
+  bold,
+  color,
   ...props
 }: PropsWithChildren<PropsWithStyleOverrides<TextProps>>) => {
   const fontSizes = {
@@ -106,15 +121,25 @@ export const Text = ({
       desktop: pxToRem(20),
     },
   };
-  const combinedProps = {
-    as: component,
-    variant,
-    textTransform,
-    textAlign: align,
-    fontSize: fontSizes[variant],
-    ...props,
-  };
-  return <StyledText {...combinedProps} />;
+  const { isBrandBackground } = useBackground();
+  const textColor = !!color
+    ? color
+    : isBrandBackground
+    ? colorsCommon.brandWhite
+    : colorsCommon.brandMidnight;
+
+  return (
+    <Typography
+      component={component}
+      fontFamily="secondary"
+      fontSize={fontSizes[variant]}
+      lineHeight={variant === 'caption' ? 2 : 1.5}
+      fontWeight={bold ? 'semibold' : 'regular'}
+      textAlign={align}
+      color={textColor}
+      {...props}
+    />
+  );
 };
 
 Text.displayName = displayName;
