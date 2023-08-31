103cb4619fd8695c987ec912062ab970270971be
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2023-08-31 16:41:10 +0100

    add `Em` component (#170)

---
+++ packages/web-ui/src/Em/Em.props.ts
@@ -0,0 +1,5 @@
+import { TypographyProps } from '../Typography';
+
+export interface EmProps
+  extends Pick<TypographyProps, 'textTransform' | 'padding' | 'margin'>,
+    React.ComponentPropsWithoutRef<'em'> {}
