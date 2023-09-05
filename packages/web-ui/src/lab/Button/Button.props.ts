341cb2848d4172a6cff204cff44b2815b1d79938
Rob Phoenix <rphoenix@uw.co.uk>
2023-09-05 13:53:22 +0100

    update styles

--- packages/web-ui/src/lab/Button/Button.props.ts
+++ packages/web-ui/src/lab/Button/Button.props.ts
@@ -1,3 +1,30 @@
 import { ComponentPropsWithoutRef } from 'react';
 
-export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {}
+type SolidColorSchemeProps = {
+  variant?: 'solid';
+  colorScheme?: 'cyan' | 'red' | 'green';
+};
+
+type OutlineColorSchemeProps = {
+  variant?: 'outline';
+  colorScheme?: 'cyan' | 'grey' | 'red' | 'green' | 'gold';
+};
+
+export type ButtonProps = ComponentPropsWithoutRef<'button'> &
+  (SolidColorSchemeProps | OutlineColorSchemeProps) & {
+    /**
+     * Sets the button's visual variant
+     * @default solid
+     */
+    // variant?: 'solid' | 'outline' | 'ghost';
+    /**
+     * Sets the button's colour scheme
+     * @default cyan
+     */
+    // colorScheme?: 'cyan';
+    /**
+     * Sets the button height.
+     * @default large
+     */
+    size?: 'small' | 'large';
+  };
