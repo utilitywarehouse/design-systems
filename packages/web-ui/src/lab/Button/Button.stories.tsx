04d11f5deb77ceb61df2860e624e07bb73ab726d
Rob Phoenix <rphoenix@uw.co.uk>
2023-09-05 13:53:22 +0100

    update styles

--- packages/web-ui/src/lab/Button/Button.stories.tsx
+++ packages/web-ui/src/lab/Button/Button.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Flex } from '../../Flex';
+import { Heading } from '../../Heading';
 import { Button } from './Button';
 
 const meta: Meta<typeof Button> = {
@@ -9,6 +11,46 @@ const meta: Meta<typeof Button> = {
 
 export default meta;
 type Story = StoryObj<typeof Button>;
+
+const sizes = ['large', 'small'] as const;
+const colorSchemes = {
+  solid: ['cyan', 'red', 'green'] as const,
+  outline: ['cyan', 'grey', 'red', 'green', 'gold'] as const,
+};
+
+export const KitchenSink: Story = {
+  parameters: { controls: { hideNoControlsWarning: true } },
+  render: () => {
+    return (
+      <Flex direction="column" gap={2}>
+        <Heading>Solid</Heading>
+        <Flex gap={3}>
+          {sizes.map(size => (
+            <Flex gap={2}>
+              {colorSchemes.solid.map(colorScheme => (
+                <Button key={colorScheme} variant="solid" colorScheme={colorScheme} size={size}>
+                  Button
+                </Button>
+              ))}
+            </Flex>
+          ))}
+        </Flex>
+        <Heading>Outline</Heading>
+        <Flex gap={3}>
+          {sizes.map(size => (
+            <Flex gap={2}>
+              {colorSchemes.outline.map(colorScheme => (
+                <Button key={colorScheme} variant="outline" colorScheme={colorScheme} size={size}>
+                  Button
+                </Button>
+              ))}
+            </Flex>
+          ))}
+        </Flex>
+      </Flex>
+    );
+  },
+};
 
 export const Workshop: Story = {
   argTypes: {
