5311f034e5f090c2054bb6221acc6077e541c965
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2023-09-07 11:46:47 +0100

    add docs (#179)

--- packages/web-ui/src/Box/Box.stories.tsx
+++ packages/web-ui/src/Box/Box.stories.tsx
@@ -5,6 +5,7 @@ import { useRef } from 'react';
 import { fonts } from '../tokens';
 import { colorsCommon } from '@utilitywarehouse/colour-system';
 import { Text } from '../Text';
+import '@utilitywarehouse/colour-system/css/colours.css';
 
 const meta: Meta<typeof Box> = {
   title: 'Web UI / Layout / Box',
