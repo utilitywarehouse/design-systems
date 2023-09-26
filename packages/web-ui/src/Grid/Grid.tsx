812e6cc52969045eb4b92d92a05d317cd4a8f078
bess <espencer@uw.co.uk>
2023-09-26 15:49:08 +0100

    Replace material  with the one from system

--- packages/web-ui/src/Grid/Grid.tsx
+++ packages/web-ui/src/Grid/Grid.tsx
@@ -1,7 +1,8 @@
-import MuiGrid from '@mui/material/Grid';
+import MuiGrid from '@mui/system/Unstable_Grid';
 import type { OverridableComponent, OverrideProps } from '@mui/material/OverridableComponent';
 import { forwardRef } from 'react';
-import type { GridProps as MuiGridProps, RegularBreakpoints } from '@mui/material/Grid';
+import type { GridProps as MuiGridProps } from '@mui/system/Unstable_Grid';
+import { RegularBreakpoints } from '@mui/material/Grid';
 
 export const DEFAULT_COLUMNS = { mobile: 4, tablet: 8, desktop: 12, wide: 12 };
 export const DEFAULT_SPACING = { mobile: 2, tablet: 3, desktop: 3, wide: 3 };
@@ -29,10 +30,10 @@ export type GridProps<D extends React.ElementType = DefaultGridComponent, P = {}
  * A responsive layout grid which adapts to screen size and orientation, ensuring
  * consistency across layouts.
  *
- * This component is based on the `@mui/material` Grid component, except it
+ * This component is based on the `@mui/system` Grid component, except it
  * adheres to our custom breakpoints, and has default spacing & columns.
  *
- * Please [check the MUI site for further documentation](https://mui.com/material-ui/react-grid/).
+ * Please [check the MUI site for further documentation](https://mui.com/system/react-grid/).
  */
 export const Grid = forwardRef(function Grid({ columns = DEFAULT_COLUMNS, ...props }, ref) {
   if (props.container) {
