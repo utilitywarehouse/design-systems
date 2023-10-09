8ab2dc3c61509e42110f324d6721ca154eb2ba5e
Rob Phoenix <rphoenix@uw.co.uk>
2023-10-09 20:51:50 +0100

    use a more terse global prefix

--- packages/web-ui/src/utils/utils.ts
+++ packages/web-ui/src/utils/utils.ts
@@ -2,5 +2,5 @@ import { spacingBase } from '../tokens';
 
 export const px = (value: string | number): string => `${value}px`;
 export const spacing = (multiplier: number) => multiplier * spacingBase;
-export const globalPrefix = 'uw-web-ui';
+export const globalPrefix = 'uwu';
 export const getClassName = (className: string) => `${globalPrefix}-${className}`;
