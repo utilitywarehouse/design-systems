db6eb0e5c34171e75662ee0a7898b063db05e449
Rob Phoenix <rphoenix@uw.co.uk>
2023-10-31 14:57:07 +0000

    add responsive size prop and focus styles

--- packages/web-ui/src/lab/Link/Link.stories.tsx
+++ packages/web-ui/src/lab/Link/Link.stories.tsx
@@ -1,6 +1,13 @@
+import * as React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { ChevronRightMediumIcon } from '@utilitywarehouse/react-icons';
+import {
+  ChevronRightMediumIcon,
+  ChevronRightSmallIcon,
+  ChevronLeftMediumIcon,
+  ChevronLeftSmallIcon,
+} from '@utilitywarehouse/react-icons';
 import { Link } from './Link';
+import { Flex } from '../../Flex';
 
 const meta: Meta<typeof Link> = {
   title: 'Web UI / Lab / Link',
@@ -10,11 +17,39 @@ const meta: Meta<typeof Link> = {
 export default meta;
 type Story = StoryObj<typeof Link>;
 
+const sizes = ['large', 'small'] as const;
+
 export const Workshop: Story = {
   render: () => (
-    <Link>
-      Link
-      <ChevronRightMediumIcon />
-    </Link>
+    <Flex direction="column" gap={4}>
+      <Flex gap={2} align="center">
+        {sizes.map(size => (
+          <Link key={size} size={size} href="#">
+            Link
+            {size === 'large' ? <ChevronRightMediumIcon /> : <ChevronRightSmallIcon />}
+          </Link>
+        ))}
+      </Flex>
+      <Flex gap={2} align="center">
+        {sizes.map(size => (
+          <Link key={size} size={size} href="#">
+            {size === 'large' ? <ChevronLeftMediumIcon /> : <ChevronLeftSmallIcon />}
+            Link
+          </Link>
+        ))}
+      </Flex>
+    </Flex>
   ),
+};
+
+export const ResponsiveSize: Story = {
+  args: {
+    children: 'Responsive size link',
+    size: {
+      mobile: 'large',
+      tablet: 'small',
+      desktop: 'large',
+      wide: 'small',
+    },
+  },
 };
--- packages/web-ui/src/lab/Link/index.ts
+++ packages/web-ui/src/lab/Link/index.ts
@@ -1,2 +1,2 @@
-export { Button } from './Button';
-export type { ButtonProps } from './Button.props';
+export { Link } from './Link';
+export type { LinkProps } from './Link.props';
