eb330f654bea6d02b49c93168c712673ba3b8373
Rob Phoenix <rphoenix@uw.co.uk>
2023-11-13 09:51:03 +0000

    add colour migration doc

--- packages/web-ui/.storybook/main.js
+++ packages/web-ui/.storybook/main.js
@@ -1,13 +1,20 @@
+import remarkGfm from 'remark-gfm';
+
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   stories: ['../docs/**/*.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
     {
-      name: '@storybook/addon-essentials',
+      name: '@storybook/addon-docs',
       options: {
-        actions: false,
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
       },
     },
   ],
