cf0e1d5ccf86a77499fb3a5a0ffdaa53a24d7feb
Rob Phoenix <rphoenix@uw.co.uk>
2023-11-21 11:36:56 +0000

    add icons to HelperText

--- packages/web-ui/src/HelperText/HelperText.stories.tsx
+++ packages/web-ui/src/HelperText/HelperText.stories.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { HelperText } from './HelperText';
 
@@ -5,22 +6,23 @@ const meta: Meta<typeof HelperText> = {
   title: 'Web UI / Typography / HelperText',
   component: HelperText,
   tags: ['autodocs'],
+  argTypes: {
+    children: { control: { type: 'text' } },
+    disabled: { control: { type: 'boolean' } },
+    showIcon: { control: { type: 'boolean' } },
+    validationStatus: { control: { type: 'radio' }, options: ['initial', 'valid', 'invalid'] },
+  },
+  args: {
+    children: 'Helper text',
+    disabled: false,
+    showIcon: false,
+    validationStatus: 'initial',
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof HelperText>;
 
 export const Workshop: Story = {
-  argTypes: {
-    children: { control: { type: 'text' } },
-    disabled: { control: { type: 'boolean' } },
-    error: { control: { type: 'boolean' } },
-    validationStatus: { control: { type: 'radio' }, options: ['default', 'valid', 'invalid'] },
-  },
-  args: {
-    children: 'Helper text',
-    disabled: false,
-    error: false,
-    validationStatus: undefined,
-  },
+  render: args => <HelperText {...args} />,
 };
--- packages/web-ui/src/HelperText/HelperText.tsx
+++ packages/web-ui/src/HelperText/HelperText.tsx
@@ -7,39 +7,54 @@ import { Typography } from '../Typography';
 import { HelperTextProps } from './HelperText.props';
 import clsx from 'clsx';
 import { styled } from '../theme';
+import { Flex } from '../Flex';
+import {
+  Information01SmallContainedIcon,
+  Warning01SmallContainedIcon,
+  Tick01SmallContainedIcon,
+} from '@utilitywarehouse/react-icons';
 
 const componentName = 'HelperText';
 const componentClassName = getPrefixedName(componentName);
 
 const classNames = {
-  status: {
-    valid: getPrefixedName('status-valid'),
-    invalid: getPrefixedName('status-invalid'),
-  },
+  valid: getPrefixedName('valid'),
+  invalid: getPrefixedName('invalid'),
 };
 
 const classSelectors = {
-  status: {
-    valid: classSelector(classNames.status.valid),
-    invalid: classSelector(classNames.status.invalid),
-  },
+  valid: classSelector(classNames.valid),
+  invalid: classSelector(classNames.invalid),
 };
 
-const StyledTypography = styled(Typography)<HelperTextProps>({
-  '--helper-text-color-default': colors.grey800,
+const StyledFlex = styled(Flex)({
+  '--helper-text-color-initial': colors.grey800,
   '--helper-text-color-disabled': colors.grey400,
   '--helper-text-color-valid': colors.green600,
   '--helper-text-color-invalid': colors.red600,
-  '--helper-text-color': 'var(--helper-text-color-default)',
+  '--helper-text-color': 'var(--helper-text-color-initial)',
+  '--helper-text-icon-color-initial': colors.grey700,
+  '--helper-text-icon-color-disabled': colors.grey300,
+  '--helper-text-icon-color-valid': colors.green500,
+  '--helper-text-icon-color-invalid': colors.red500,
+  '--helper-text-icon-color': 'var(--helper-text-icon-color-initial)',
+  '> :where(svg)': {
+    // as UW icons use currentColor by default, this will fallback to the
+    // Button's color property if not set.
+    color: 'var(--helper-text-icon-color)',
+  },
   color: 'var(--helper-text-color)',
   [dataAttributes.disabled]: {
     '--helper-text-color': 'var(--helper-text-color-disabled)',
+    '--helper-text-icon-color': 'var(--helper-text-icon-color-disabled)',
   },
-  [classSelectors.status.valid]: {
+  [classSelectors.valid]: {
     '--helper-text-color': 'var(--helper-text-color-valid)',
+    '--helper-text-icon-color': 'var(--helper-text-icon-color-valid)',
   },
-  [classSelectors.status.invalid]: {
+  [classSelectors.invalid]: {
     '--helper-text-color': 'var(--helper-text-color-invalid)',
+    '--helper-text-icon-color': 'var(--helper-text-icon-color-invalid)',
   },
 });
 
@@ -56,23 +71,37 @@ const StyledTypography = styled(Typography)<HelperTextProps>({
 export const HelperText = forwardRef<
   ElementRef<'span'>,
   PropsWithChildren<PropsWithSx<HelperTextProps>>
->(({ disabled, validationStatus = 'default', className, ...props }, ref) => {
+>(({ showIcon, validationStatus = 'initial', disabled, className, ...props }, ref) => {
+  const icons: { [key: string]: any } = {
+    initial: Information01SmallContainedIcon,
+    valid: Tick01SmallContainedIcon,
+    invalid: Warning01SmallContainedIcon,
+  };
+  const Icon = icons[validationStatus];
   return (
-    <StyledTypography
+    <StyledFlex
       ref={ref}
       component="span"
+      direction="row"
+      gap={1}
       className={clsx(
         componentClassName,
-        validationStatus && validationStatus !== 'default' && classNames.status[validationStatus],
+        validationStatus && validationStatus !== 'initial' && classNames[validationStatus],
         className
       )}
-      fontFamily="secondary"
-      weight="regular"
-      fontSize={pxToRem(13)}
-      lineHeight={pxToRem(16)}
       data-disabled={disabled || undefined}
-      {...props}
-    />
+    >
+      {showIcon ? <Icon /> : null}
+      <Typography
+        ref={ref}
+        component="span"
+        fontFamily="secondary"
+        weight="regular"
+        fontSize={pxToRem(13)}
+        lineHeight={pxToRem(16)}
+        {...props}
+      />
+    </StyledFlex>
   );
 });
 
