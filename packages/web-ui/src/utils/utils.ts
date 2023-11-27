23ac8e6cd02454bf100bfd78132a5eaa51852c88
Rob Phoenix <rphoenix@uw.co.uk>
2023-11-27 15:54:37 +0000

    rename global prefix

--- packages/web-ui/src/utils/utils.ts
+++ packages/web-ui/src/utils/utils.ts
@@ -1,10 +1,12 @@
 import { spacingBase } from '../tokens';
 import { Breakpoints } from '../types';
 
+export const GLOBAL_PREFIX = 'uwu';
+
 export const px = (value: string | number): string => `${value}px`;
 export const spacing = (multiplier: number) => multiplier * spacingBase;
-export const globalPrefix = 'uwu';
-export const getPrefixedName = (name: string) => `${globalPrefix}-${name}`;
+
+export const getPrefixedName = (name: string) => `${GLOBAL_PREFIX}-${name}`;
 export const classSelector = (className: string) => `&:where(.${className})`;
 export const responsiveClassSelector = (className: string, breakpoint: Breakpoints) =>
   `&:where(.${breakpoint}\\:${className})`;
