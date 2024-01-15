fcc2b39f47035ab0ba550c5fc8ac11a24b9b1f31
Jord McCord <jmccord@uw.co.uk>
2024-01-15 16:31:49 +0000

    feat: add 600/400 step for green, red, gold (#235)

--- packages/colour-system/src/light/green.ts
+++ packages/colour-system/src/light/green.ts
@@ -8,6 +8,8 @@ export const green100 = '#e0f5e8';
 export const green200 = '#bdead0';
 /**  */
 export const green300 = '#a3e1bf';
+/**  */
+export const green400 = '#2fb66d';
 /** Employ it for borders, icons, and exclusively for large text against white, Green 50, Grey 25, and Grey 50. Use it as a background colour, pairing it with white, for instance, in buttons. It's ideal for icons placed next to text in green 600. However, it's not the most suitable choice for small text; for that, stick with green 600. */
 export const green500 = '#1c874d';
 /** For text, icons and border against white, Green 50 and Green 100 For text, icons and border against white Green 50, Green 100 and Green 200, Grey 25, Grey 50 and Grey 100. For backgrounds with a white foreground.  */
--- packages/colour-system/src/light/red.ts
+++ packages/colour-system/src/light/red.ts
@@ -8,6 +8,8 @@ export const red100 = '#ffd8d9';
 export const red200 = '#ffb3b4';
 /**  */
 export const red300 = '#f58e92';
+/**  */
+export const red400 = '#ec515d';
 /** Use it for borders, icons, and large text against white, Red 50, Red 100, Grey 25, Grey 50, Grey 75, and Grey 100. Use it as a background color with white as the foreground color. */
 export const red500 = '#df2a38';
 /** For text. It passes AA up to Grey 100 */
