396316d9bd743b2670ac1f5b170267809eac62ed
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2024-02-09 16:13:09 +0000

    remove playground app (#242)

--- apps/playground/src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import '@utilitywarehouse/css-reset';
-import '@utilitywarehouse/fontsource';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
