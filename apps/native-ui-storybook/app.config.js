1e12367dd0201b803e7bd96af2ea11ec6e7b9fda
Jord McCord <jmccord@uw.co.uk>
2024-02-13 13:55:14 +0000

    chore: set up expo build

--- apps/native-ui-storybook/app.config.js
+++ apps/native-ui-storybook/app.config.js
@@ -1,32 +1,51 @@
 export default ({ config }) => ({
   ...config,
-  name: "nativeuistorybook",
-  slug: "expo-template-blank-typescript",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
+  name: 'Native UI',
+  slug: 'native-ui',
+  scheme: 'native-ui',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
   extra: {
     storybookEnabled: process.env.STORYBOOK_ENABLED,
+    eas: {
+      projectId: '5d5d04e2-5607-4d40-84a9-4d1bdf331ae3',
+    },
   },
   splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
   },
   updates: {
     fallbackToCacheTimeout: 0,
   },
-  assetBundlePatterns: ["**/*"],
+  assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: true,
+    bundleIdentifier: 'com.utilitywarehouse.native-ui',
+    infoPlist: {
+      LSApplicationQueriesSchemes: ['native-ui'],
+    },
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon.png",
-      backgroundColor: "#FFFFFF",
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
     },
   },
   web: {
-    favicon: "./assets/favicon.png",
+    favicon: './assets/favicon.png',
+  },
+  build: {
+    preview: {
+      env: {
+        STORYBOOK_ENABLED: 'true',
+      },
+      ios: {
+        simulator: true,
+      },
+    },
+    production: {},
   },
 });
