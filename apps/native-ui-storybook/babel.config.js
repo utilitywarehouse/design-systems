9a2a862a5da0f3d116a3268b633679bca758ac1d
Jord McCord <jmccord@uw.co.uk>
2024-02-26 15:37:50 +0000

    chore: update deps

--- apps/native-ui-storybook/babel.config.js
+++ apps/native-ui-storybook/babel.config.js
@@ -1,19 +1,7 @@
-const path = require('path');
-
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@utilitywarehouse/native-ui': path.join(__dirname, '../../packages/native-ui/src'),
-          },
-        },
-      ],
-      ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
-    ],
+    plugins: [['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }]],
   };
 };
