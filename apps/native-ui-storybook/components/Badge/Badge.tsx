4ad9f1cc9f94dab3f75482f6d3ddca3683f920e1
Jord McCord <jmccord@uw.co.uk>
2024-03-05 10:39:46 +0000

    feat: add badge variants

--- apps/native-ui-storybook/components/Badge/Badge.tsx
+++ apps/native-ui-storybook/components/Badge/Badge.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { Badge, BadgeText, BadgeIcon } from '@utilitywarehouse/native-ui';
 import { AddSmallIcon } from '@utilitywarehouse/react-native-icons';
+import { Meta, StoryFn } from '@storybook/react';
 
-const BadgeBasic = ({ text = 'NEW FEATURE', icon = false, ...props }: any) => {
+const BadgeBasic: StoryFn = ({ text = 'NEW FEATURE', icon = false, ...props }: any) => {
   return (
-    <Badge {...props} gap="$1">
+    <Badge {...props}>
       <BadgeText>{text}</BadgeText>
       {icon && <BadgeIcon as={AddSmallIcon} />}
     </Badge>
   );
 };
 
-BadgeBasic.description =
-  'This is a basic Badge component example. The badge component lets you quickly and easily add status indicators to your interface for improved usability. They are designed to be attention-grabbing and quickly convey important information.';
+BadgeBasic.argTypes = {
+  colorScheme: {
+    control: 'select',
+    options: ['cyan', 'red', 'green', 'gold', 'grey'],
+  },
+  borderless: {
+    control: 'boolean',
+  },
+  strong: {
+    control: 'boolean',
+  },
+  size: {
+    control: 'select',
+    options: ['large', 'small'],
+  },
+} as Meta<typeof Badge>['argTypes'];
+
+BadgeBasic.args = {
+  text: 'New Feature',
+  strong: false,
+  borderless: false,
+  colorScheme: 'cyan',
+  size: 'large',
+} as Meta<typeof Badge>['args'];
 
 export default BadgeBasic;
