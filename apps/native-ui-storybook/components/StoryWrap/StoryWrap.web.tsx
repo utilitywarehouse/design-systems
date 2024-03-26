188a1113125bb5f09396e36712e26230f6b16a5e
Jord McCord <jmccord@uw.co.uk>
2024-03-26 14:58:18 +0000

    chore:  build preview for each pr

--- apps/native-ui-storybook/components/StoryWrap/StoryWrap.web.tsx
+++ apps/native-ui-storybook/components/StoryWrap/StoryWrap.web.tsx
@@ -2,6 +2,9 @@ import React, { FC, PropsWithChildren, useContext } from 'react';
 import { EmulatorRenderer } from '@storybook/native-components';
 import { PlatformContext } from '../../contexts/PlatformContext';
 
+const ANDROID_API_KEY = process.env.ANDROID_API_KEY; // || 'ncapxlszjomiyae42o3hr2hr34';
+const IOS_API_KEY = process.env.IOS_API_KEY; // || 'w7k6nlib4xevw7fxrqp6now7iu';
+
 const StoryWrap: FC<PropsWithChildren> = ({ children }) => {
   const { platform, id, args, colourMode } = useContext(PlatformContext);
 
@@ -9,7 +12,7 @@ const StoryWrap: FC<PropsWithChildren> = ({ children }) => {
     <EmulatorRenderer
       platform={platform as 'android' | 'ios'}
       deepLinkBaseUrl="native-ui://story"
-      apiKey={platform === 'android' ? 'ncapxlszjomiyae42o3hr2hr34' : 'w7k6nlib4xevw7fxrqp6now7iu'}
+      apiKey={(platform === 'android' ? ANDROID_API_KEY : IOS_API_KEY) || ''}
       storyParams={{ storyId: id, colourMode }}
       extraParams={{ ...args }}
     />
