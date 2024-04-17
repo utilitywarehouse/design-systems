3e2941799c4a39e640765913e0d370cd632a43ad
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2024-04-17 13:02:34 +0100

    Update lab/Button types to guard against dead prop combinations. (#273)

---
+++ .changeset/silly-doors-rule.md
@@ -0,0 +1,5 @@
+---
+'@utilitywarehouse/web-ui': patch
+---
+
+Update lab/Button types to guard against dead prop combinations.
--- packages/web-ui/src/BaseButton/BaseButton.props.ts
+++ packages/web-ui/src/BaseButton/BaseButton.props.ts
@@ -1,14 +1,21 @@
 import { UnstyledButtonProps } from '../UnstyledButton';
 
-export type BaseButtonProps = UnstyledButtonProps & {
-  /**
-   * Sets the button's visual variant
-   * @default solid
-   */
-  variant?: 'solid' | 'outline' | 'ghost';
-  /**
-   * Sets the button's colour scheme
-   * @default cyan
-   */
-  colorScheme?: 'cyan' | 'red' | 'green' | 'gold' | 'grey';
-};
+export type BaseButtonProps = UnstyledButtonProps &
+  (
+    | {
+        /**
+         * Sets the button's visual variant
+         * @default solid
+         */
+        variant?: 'solid';
+        /**
+         * Sets the button's colour scheme
+         * @default cyan
+         */
+        colorScheme?: 'cyan' | 'red' | 'green';
+      }
+    | {
+        variant?: 'outline' | 'ghost';
+        colorScheme?: 'cyan' | 'red' | 'green' | 'gold' | 'grey';
+      }
+  );
