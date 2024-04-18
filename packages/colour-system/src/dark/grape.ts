e4a0ac3268ff2da4eb0a59f7d377fa61685dc60a
robphoenix <robphoenix@users.noreply.github.com>
2024-04-18 13:43:11 +0000

    Generate updated colour system

--- packages/colour-system/src/dark/grape.ts
+++ packages/colour-system/src/dark/grape.ts
@@ -12,5 +12,3 @@ export const grape600 = '#a66de8';
 export const grape700 = '#bc92ed';
 /** Use is as any foreground against Grape 100, Grape 200 or any Grey shade in the range of 50 to 300. */
 export const grape800 = '#d4baf3';
-/** Use is as any foreground against Grape 100, Grape 200 or any Grey shade in the range of 50 to 300. */
-export const grape950 = '#f1e6fb';
