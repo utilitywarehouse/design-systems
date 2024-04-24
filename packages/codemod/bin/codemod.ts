3b64833c6f8dceb94a2099b1c1010c86d95daa38
Rob Phoenix <rphoenix@uw.co.uk>
2024-04-24 10:06:27 +0100

    I think it works now?

--- packages/codemod/bin/codemod.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-// @utilitywarehouse/web-ui-codemod optional-name-of-transform optional/path/to/src [...options]
-
-import cli = require('./cli');
-cli.run();
--- packages/codemod/transforms/web-ui/v0/__tests__/import-paths.test.ts
+++
@@ -1,9 +0,0 @@
-import { defineTest } from 'jscodeshift/src/testUtils';
-
-const NAME = 'import-paths';
-
-describe(NAME, () => {
-  defineTest(__dirname, NAME, {}, undefined, {
-    parser: 'tsx',
-  });
-});
