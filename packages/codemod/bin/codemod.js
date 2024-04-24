3204d8bfa9629c4df551a5794556c1e7ecaf1d23
Rob Phoenix <rphoenix@uw.co.uk>
2024-04-24 14:42:04 +0100

    rip & replace codemod cli

---
+++ packages/codemod/bin/codemod.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+
+const childProcess = require('child_process');
+const { promises: fs } = require('fs');
+const path = require('path');
+const yargs = require('yargs');
+const jscodeshiftPackage = require('jscodeshift/package.json');
+
+const jscodeshiftDirectory = path.dirname(require.resolve('jscodeshift'));
+const jscodeshiftExecutable = path.join(jscodeshiftDirectory, jscodeshiftPackage.bin.jscodeshift);
+
+async function runJscodeshiftTransform(transform, files, flags, codemodFlags) {
+  const path = path.resolve(__dirname, '..', './transforms', `${transform}.js`);
+
+  let transformerPath;
+  let error;
+  // eslint-disable-next-line no-restricted-syntax
+  try {
+    // eslint-disable-next-line no-await-in-loop
+    await fs.stat(item);
+    error = undefined;
+    transformerPath = item;
+  } catch (srcPathError) {
+    error = srcPathError;
+  }
+
+  if (error) {
+    if (error?.code === 'ENOENT') {
+      throw new Error(
+        `Transform '${transform}' not found. Check out ${path.resolve(
+          __dirname,
+          './README.md for a list of available codemods.'
+        )}`
+      );
+    }
+    throw error;
+  }
+
+  const args = [
+    // can't directly spawn `jscodeshiftExecutable` due to https://github.com/facebook/jscodeshift/issues/424
+    jscodeshiftExecutable,
+    '--transform',
+    transformerPath,
+    ...codemodFlags,
+    '--extensions',
+    'js,ts,jsx,tsx,json',
+    '--parser',
+    'tsx',
+    '--ignore-pattern',
+    '**/node_modules/**',
+  ];
+
+  if (flags.dry) {
+    args.push('--dry');
+  }
+  if (flags.print) {
+    args.push('--print');
+  }
+  if (flags.jscodeshift) {
+    args.push(flags.jscodeshift);
+  }
+
+  args.push(...files);
+
+  // eslint-disable-next-line no-console -- debug information
+  console.log(`Executing command: jscodeshift ${args.join(' ')}`);
+  const jscodeshiftProcess = childProcess.spawnSync('node', args, { stdio: 'inherit' });
+
+  if (jscodeshiftProcess.error) {
+    throw jscodeshiftProcess.error;
+  }
+}
+
+function run(argv) {
+  const { codemod, paths, ...flags } = argv;
+  const files = paths.map(filePath => path.resolve(filePath));
+
+  runJscodeshiftTransform(codemod, files, flags, argv._);
+}
+
+yargs
+  .command({
+    command: '$0 <codemod> <paths...>',
+    describe: 'Applies a `@utilitywarehouse/ds-codemod` to the specified paths',
+    builder: command => {
+      return command
+        .positional('codemod', {
+          description: 'The name of the codemod',
+          type: 'string',
+        })
+        .positional('paths', {
+          array: true,
+          description: 'Paths forwarded to `jscodeshift`',
+          type: 'string',
+        })
+        .option('dry', {
+          description: 'dry run (no changes are made to files)',
+          default: false,
+          type: 'boolean',
+        })
+        .option('print', {
+          description: 'print transformed files to stdout, useful for development',
+          default: false,
+          type: 'boolean',
+        });
+    },
+    handler: run,
+  })
+  .scriptName('npx @utilitywarehouse/ds-codemod')
+  .example('$0 web-ui/v0/import-paths src')
+  .help()
+  .parse();
