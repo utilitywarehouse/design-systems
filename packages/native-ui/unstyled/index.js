35db2ff096a355e522d8f394300c7e631be22a6a
Jord McCord <jmccord@uw.co.uk>
2024-05-02 12:17:59 +0100

    chore: add unstyled build

--- packages/native-ui/unstyled/index.js
+++ packages/native-ui/unstyled/index.js
@@ -1,106 +1,18 @@
 export { Divider } from '@gluestack-ui/themed';
-
-export {
-  FormControl,
-  FormControlError,
-  FormControlErrorIcon,
-  FormControlErrorText,
-  FormControlHelper,
-  FormControlHelperText,
-  FormControlLabel,
-  FormControlLabelText,
-} from '@gluestack-ui/themed';
-
+export { FormControl, FormControlError, FormControlErrorIcon, FormControlErrorText, FormControlHelper, FormControlHelperText, FormControlLabel, FormControlLabelText, } from '@gluestack-ui/themed';
 export { Input, InputField, InputIcon, InputSlot } from '@gluestack-ui/themed';
-
-export {
-  Select,
-  SelectBackdrop,
-  SelectContent,
-  SelectDragIndicator,
-  SelectDragIndicatorWrapper,
-  SelectFlatList,
-  SelectIcon,
-  SelectInput,
-  SelectItem,
-  SelectItemText,
-  SelectPortal,
-  SelectScrollView,
-  SelectSectionHeaderText,
-  SelectSectionList,
-  SelectTrigger,
-  SelectVirtualizedList,
-} from '@gluestack-ui/themed';
+export { Select, SelectBackdrop, SelectContent, SelectDragIndicator, SelectDragIndicatorWrapper, SelectFlatList, SelectIcon, SelectInput, SelectItem, SelectItemText, SelectPortal, SelectScrollView, SelectSectionHeaderText, SelectSectionList, SelectTrigger, SelectVirtualizedList, } from '@gluestack-ui/themed';
 export { Slider, SliderFilledTrack, SliderThumb, SliderTrack } from '@gluestack-ui/themed';
 export { Switch } from '@gluestack-ui/themed';
 export { Textarea, TextareaInput } from '@gluestack-ui/themed';
-export {
-  Avatar,
-  AvatarBadge,
-  AvatarFallbackText,
-  AvatarGroup,
-  AvatarImage,
-} from '@gluestack-ui/themed';
-
+export { Avatar, AvatarBadge, AvatarFallbackText, AvatarGroup, AvatarImage, } from '@gluestack-ui/themed';
 export { Progress, ProgressFilledTrack } from '@gluestack-ui/themed';
-
 export { Toast, ToastDescription, ToastTitle, useToast } from '@gluestack-ui/themed';
-
-export {
-  AlertDialog,
-  AlertDialogBackdrop,
-  AlertDialogBody,
-  AlertDialogCloseButton,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-} from '@gluestack-ui/themed';
-
+export { AlertDialog, AlertDialogBackdrop, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, } from '@gluestack-ui/themed';
 export { Menu, MenuItem, MenuItemLabel } from '@gluestack-ui/themed';
-export {
-  Modal,
-  ModalBackdrop,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-} from '@gluestack-ui/themed';
-export {
-  Popover,
-  PopoverArrow,
-  PopoverBackdrop,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverFooter,
-  PopoverHeader,
-} from '@gluestack-ui/themed';
+export { Modal, ModalBackdrop, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, } from '@gluestack-ui/themed';
+export { Popover, PopoverArrow, PopoverBackdrop, PopoverBody, PopoverCloseButton, PopoverContent, PopoverFooter, PopoverHeader, } from '@gluestack-ui/themed';
 export { Tooltip, TooltipContent, TooltipText } from '@gluestack-ui/themed';
-export {
-  Actionsheet,
-  ActionsheetBackdrop,
-  ActionsheetContent,
-  ActionsheetDragIndicator,
-  ActionsheetDragIndicatorWrapper,
-  ActionsheetFlatList,
-  ActionsheetIcon,
-  ActionsheetItem,
-  ActionsheetItemText,
-  ActionsheetScrollView,
-  ActionsheetSectionHeaderText,
-  ActionsheetSectionList,
-  ActionsheetVirtualizedList,
-} from '@gluestack-ui/themed';
-export {
-  Accordion,
-  AccordionIcon,
-  AccordionContent,
-  AccordionContentText,
-  AccordionHeader,
-  AccordionItem,
-  AccordionTitleText,
-  AccordionTrigger,
-} from '@gluestack-ui/themed';
-
+export { Actionsheet, ActionsheetBackdrop, ActionsheetContent, ActionsheetDragIndicator, ActionsheetDragIndicatorWrapper, ActionsheetFlatList, ActionsheetIcon, ActionsheetItem, ActionsheetItemText, ActionsheetScrollView, ActionsheetSectionHeaderText, ActionsheetSectionList, ActionsheetVirtualizedList, } from '@gluestack-ui/themed';
+export { Accordion, AccordionIcon, AccordionContent, AccordionContentText, AccordionHeader, AccordionItem, AccordionTitleText, AccordionTrigger, } from '@gluestack-ui/themed';
 export { Fab, FabIcon, FabLabel } from '@gluestack-ui/themed';
