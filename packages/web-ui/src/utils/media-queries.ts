01f8bd2d30959ec2bedb19c6098db936179461a5
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2024-05-07 14:29:03 +0100

    rphoenix uwds 2266 (#317)

--- packages/web-ui/src/utils/media-queries.ts
+++ packages/web-ui/src/utils/media-queries.ts
@@ -2,6 +2,7 @@ import { breakpoints } from '../tokens';
 
 const unit = 'px';
 const mq = (breakpoint: number) => `@media (min-width:${breakpoint}${unit})`;
+/* mobile first media queries mirroring the Web UI breakpoint values */
 export const mediaQueries = {
   mobile: mq(breakpoints.mobile),
   tablet: mq(breakpoints.tablet),
