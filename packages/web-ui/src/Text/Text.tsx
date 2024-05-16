d61196fae588946adb2c9754deae72e876bdd6eb
Rob Phoenix <rphoenix@uw.co.uk>
2024-05-16 13:43:15 +0100

    Update Text

--- packages/web-ui/src/Text/Text.tsx
+++ packages/web-ui/src/Text/Text.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useBackground } from '../Box';
+import { createBox, useBackground } from '../Box';
 import { colorsCommon } from '@utilitywarehouse/colour-system';
 import {
   DATA_ATTRIBUTES,
@@ -9,15 +9,14 @@ import {
   pxToRem,
   withGlobalPrefix,
 } from '../utils';
-import { Typography } from '../Typography';
 import { TextProps } from './Text.props';
 import { PropsWithSx } from '../types';
 import clsx from 'clsx';
 import { styled } from '../theme';
-import { fontWeights } from '../tokens';
+import { fontWeights, fonts } from '../tokens';
 
 const componentName = 'Text';
-const componentClassName = withGlobalPrefix(componentName);
+const BaseBox = createBox<'p' | 'span'>({ componentName });
 
 const classNames = {
   bold: withGlobalPrefix('bold'),
@@ -39,10 +38,11 @@ const classSelectors = {
   },
 };
 
-const StyledElement = styled(Typography, { shouldForwardProp: prop => prop !== 'color' })<{
+const StyledElement = styled(BaseBox, { shouldForwardProp: prop => prop !== 'color' })<{
   color?: string;
 }>(({ color }) => {
   return {
+    fontFamily: fonts.secondary,
     fontSize: 'var(--text-font-size)',
     lineHeight: 'var(--text-line-height)',
     fontWeight: 'var(--text-font-weight)',
@@ -114,10 +114,7 @@ export const Text = React.forwardRef<
   return (
     <StyledElement
       ref={ref}
-      className={clsx(componentClassName, className, classNames.variant[variant], {
-        [classNames.bold]: bold,
-      })}
-      fontFamily="secondary"
+      className={clsx(className, classNames.variant[variant], { [classNames.bold]: bold })}
       color={color}
       {...dataAttributeProps}
       {...props}
