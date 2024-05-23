2848b5640f824ed1d683b7431814867adba8c97e
Jord McCord <jmccord@uw.co.uk>
2024-05-23 15:49:39 +0100

    fix: button variants native page

--- apps/native-ui-storybook/components/IconButton/Variants.tsx
+++ apps/native-ui-storybook/components/IconButton/Variants.tsx
@@ -10,11 +10,14 @@ import React from 'react';
 import { StoryFn } from '@storybook/react';
 
 import ButtonVariants from './components/IconButtonVariants';
+import { Dimensions, Platform } from 'react-native';
 
 const ButtonPlaygroundVariants: StoryFn = ({ size }: any) => {
   const media = useMedia();
   const { base, xs, sm } = media;
   const isMobile = base || xs || sm;
+
+  const { width, height } = Dimensions.get('window');
 
   const variants = (
     <>
@@ -26,13 +29,31 @@ const ButtonPlaygroundVariants: StoryFn = ({ size }: any) => {
     </>
   );
 
+  const nativeStyles =
+    Platform.OS !== 'web'
+      ? {
+          position: 'absolute',
+          top: -40,
+          left: -40,
+          width: width,
+          height: Platform.OS === 'android' ? height - 50 : height - 140,
+        }
+      : {};
+
   return (
     <ScrollView
       sx={{
         padding: '$4',
+        ...nativeStyles,
       }}
     >
-      {isMobile ? <VStack space="md">{variants}</VStack> : <HStack space="md">{variants}</HStack>}
+      {isMobile ? (
+        <VStack space="md" sx={{ paddingBottom: '$10' }}>
+          {variants}
+        </VStack>
+      ) : (
+        <HStack space="md">{variants}</HStack>
+      )}
     </ScrollView>
   );
 };
