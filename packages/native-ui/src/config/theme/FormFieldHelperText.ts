1263a8f5bc4126f23e7c6a7493f0e5f9761320bd
Jord McCord <jmccord@uw.co.uk>
2024-06-12 17:07:01 +0100

    fix: disabled styles

--- packages/native-ui/src/config/theme/FormFieldHelperText.ts
+++ packages/native-ui/src/config/theme/FormFieldHelperText.ts
@@ -1,9 +1,5 @@
 import { createStyle } from '@gluestack-style/react';
 
 export const FormFieldHelperText = createStyle({
-  color: '$grey800',
   fontSize: '$md',
-  _dark: {
-    color: '$darkGrey800',
-  },
 });
