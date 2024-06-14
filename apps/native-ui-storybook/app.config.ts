f39a13a344f70524904bb6e02e4d17d53367babe
Jord McCord <jmccord@uw.co.uk>
2024-06-14 10:46:51 +0100

    fix: config naming

--- apps/native-ui-storybook/app.config.ts
+++ apps/native-ui-storybook/app.config.ts
@@ -9,6 +9,36 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
+    plugins: [
+      [
+        'expo-xml-font',
+        [
+          {
+            name: 'Aeonik',
+            folder: './assets/fonts',
+            variants: [
+              { fontFile: 'aeonik_bold', fontWeight: 700 },
+              { fontFile: 'aeonik_regular', fontWeight: 400 },
+            ],
+          },
+          {
+            name: 'Work Sans',
+            folder: './assets/fonts',
+            variants: [
+              { fontFile: 'worksans_black', fontWeight: 900 },
+              { fontFile: 'worksans_bold', fontWeight: 700 },
+              { fontFile: 'worksans_extrabold', fontWeight: 800 },
+              { fontFile: 'worksans_extralight', fontWeight: 200 },
+              { fontFile: 'worksans_light', fontWeight: 300 },
+              { fontFile: 'worksans_medium', fontWeight: 500 },
+              { fontFile: 'worksans_regular', fontWeight: 400 },
+              { fontFile: 'worksans_semibold', fontWeight: 600 },
+              { fontFile: 'worksans_thin', fontWeight: 100 },
+            ],
+          },
+        ],
+      ],
+    ],
     extra: {
       storybookEnabled: 'true',
       eas: {
