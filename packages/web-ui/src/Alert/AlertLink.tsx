99d2f3299449201572257d5cdc6ce38650dc6978
Jord McCord <jmccord@uw.co.uk>
2024-06-21 09:58:50 +0100

    Update packages/web-ui/src/Alert/AlertLink.tsx
    Co-authored-by: Rob Phoenix <9257284+robphoenix@users.noreply.github.com>

--- packages/web-ui/src/Alert/AlertLink.tsx
+++ packages/web-ui/src/Alert/AlertLink.tsx
@@ -1,28 +1,45 @@
-import { styled } from '../theme';
-import { fontWeights } from '../tokens';
-import { px, pxToRem } from '../utils';
+   import * as React from 'react';
+   import { colors } from '@utilitywarehouse/colour-system';
+   import { TextLink } from '../lab/TextLink';
+   import type { TextLinkProps } from '../lab/TextLink';
+   import { styled } from '../theme';
+   import { fontWeights } from '../tokens';
+   import { COLORSCHEME_SELECTORS, withGlobalPrefix } from '../utils';
+   import { PropsWithSx } from '../types';
+   import clsx from 'clsx';
 
-const componentName = 'AlertLink';
+   const componentName = 'AlertLink';
+   const componentClassName = withGlobalPrefix(componentName);
 
-/**
- * An `AlertLink` is a component that is used to display the link of an `Alert`.
- *
- * > This component does not need to be wrapped in a `ThemeProvider` and can be used standalone with other component libraries.
- */
-export const AlertLink = styled('a')({
-  display: 'inline',
-  fontSize: pxToRem(16),
-  lineHeight: pxToRem(24),
-  fontWeight: fontWeights.secondary.semibold,
-  color: 'var(--alert-link-color)',
-  '&:focus-visible': {
-    outline: 'none',
-    borderRadius: px(4),
-    boxShadow: '0 0 0 2px var(--alert-focus-color)',
-  },
-  '&:hover': {
-    textDecoration: 'none',
-  },
-});
+  
+   const StyledElement = styled(TextLink)({
+     fontWeight: fontWeights.secondary.semibold,
+     color: 'var(--alert-link-color)',
+     textDecorationColor: 'var(--alert-link-color)',
+     [`${COLORSCHEME_SELECTORS.info} &`]: {
+       '--alert-link-color': colors.cyan700,
+     },
+     [`${COLORSCHEME_SELECTORS.success} &`]: {
+       '--alert-link-color': colors.green700,
+     },
+     [`${COLORSCHEME_SELECTORS.warning} &`]: {
+       '--alert-link-color': colors.gold700,
+     },
+     [`${COLORSCHEME_SELECTORS.error} &`]: {
+       '--alert-link-color': colors.red700,
+     },
+   });
 
-AlertLink.displayName = componentName;
+ /**
+    * An `AlertLink` is a component that is used to display the link of an `Alert`.
+    *
+    * > This component does not need to be wrapped in a `ThemeProvider` and can be used standalone with other component libraries.
+    */
+   export const AlertLink = React.forwardRef<
+     React.ElementRef<'a'>,
+     React.PropsWithChildren<PropsWithSx<TextLinkProps>>
+   >(({ className, ...props }, ref) => (
+     <StyledElement ref={ref} className={clsx(componentClassName, className)} {...props} />
+   ));
+
+   AlertLink.displayName = componentName;
