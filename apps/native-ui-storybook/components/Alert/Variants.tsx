13a7a27a11821be070d38814d15d6146e43146da
Jord McCord <jmccord@uw.co.uk>
2024-06-25 14:55:28 +0100

    chore: add variants to docs

--- apps/native-ui-storybook/components/Alert/Variants.tsx
+++ apps/native-ui-storybook/components/Alert/Variants.tsx
@@ -1,37 +1,32 @@
 import React from 'react';
 
-import { Alert, AlertIcon, AlertText, Text, VStack } from '@utilitywarehouse/native-ui';
+import { Alert, Text, VStack } from '@utilitywarehouse/native-ui';
 
 const AlertVariants = () => {
   return (
     <VStack gap={8}>
       <Text>Info - Cyan</Text>
-      <Alert colorScheme="cyan">
-        <AlertIcon />
-        <AlertText>Unlock the power of knowledge with the following information.</AlertText>
-      </Alert>
+      <Alert
+        colorScheme="cyan"
+        text="Unlock the power of knowledge with the following information."
+      />
+
       <Text>Success - Green</Text>
-      <Alert colorScheme="green">
-        <AlertIcon />
-        <AlertText>
-          Boom! You did it! Please take a moment to pat yourself on the back. You've earned it!
-        </AlertText>
-      </Alert>
+      <Alert
+        colorScheme="green"
+        text="Boom! You did it! Please take a moment to pat yourself on the back. You've earned it!"
+      />
       <Text>Error - Red</Text>
-      <Alert colorScheme="red">
-        <AlertIcon />
-        <AlertText>
-          Uh-oh! It looks like the matrix has glitched. Our team of tech ninjas are already on the
-          case. Please hold tight while we fix the issue
-        </AlertText>
-      </Alert>
+      <Alert
+        colorScheme="red"
+        text="Uh-oh! It looks like the matrix has glitched. Our team of tech ninjas are already on the
+          case. Please hold tight while we fix the issue"
+      />
       <Text>Warning - Gold</Text>
-      <Alert colorScheme="gold">
-        <AlertIcon />
-        <AlertText>
-          Warning: Reading the following content may cause spontaneous outbursts of 'aha!' moments
-        </AlertText>
-      </Alert>
+      <Alert
+        colorScheme="gold"
+        text="Warning: Reading the following content may cause spontaneous outbursts of 'aha!' moments"
+      />
     </VStack>
   );
 };
