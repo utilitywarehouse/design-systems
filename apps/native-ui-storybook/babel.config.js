4e93f5e67a5ed2b16900f095ebe2a17a298578ee
Jord McCord <jmccord@uw.co.uk>
2024-06-26 15:21:29 +0100

    fix: babel config

--- apps/native-ui-storybook/babel.config.js
+++ apps/native-ui-storybook/babel.config.js
@@ -3,16 +3,22 @@ const path = require('path');
 module.exports = function (api) {
   api.cache(true);
   return {
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@utilitywarehouse/native-ui': path.join(__dirname, '../../packages/native-ui/src'),
-          '@utilitywarehouse/native-ui/unstyled': path.join(__dirname, '../../packages/native-ui/unstyled'),
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@utilitywarehouse/native-ui': path.join(__dirname, '../../packages/native-ui/src'),
+            '@utilitywarehouse/native-ui/unstyled': path.join(
+              __dirname,
+              '../../packages/native-ui/unstyled'
+            ),
+          },
         },
-      },
+      ],
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
     ],
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin'],
   };
 };
