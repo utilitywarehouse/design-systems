73f975c84e0578a3ceccf4773189e110d972a73c
Jord McCord <jmccord@uw.co.uk>
2024-06-26 15:01:45 +0100

    fix: babel module resolution

--- apps/native-ui-storybook/babel.config.js
+++ apps/native-ui-storybook/babel.config.js
@@ -3,6 +3,15 @@ const path = require('path');
 module.exports = function (api) {
   api.cache(true);
   return {
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@utilitywarehouse/native-ui': path.join(__dirname, '../../packages/native-ui/src'),
+          '@utilitywarehouse/native-ui/unstyled': path.join(__dirname, '../../packages/native-ui/unstyled'),
+        },
+      },
+    ],
     presets: ['babel-preset-expo'],
     plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin'],
   };
