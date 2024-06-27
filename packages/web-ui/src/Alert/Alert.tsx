e37d8e15d04dbc8b33797bf69a0fcc803751324b
Rob Phoenix <rphoenix@uw.co.uk>
2024-06-27 12:24:11 +0100

    simplify Alert text & title

--- packages/web-ui/src/Alert/Alert.tsx
+++ packages/web-ui/src/Alert/Alert.tsx
@@ -31,6 +31,7 @@ const StyledElement = styled('div')({
   flexDirection: 'row',
   padding: px(16),
   backgroundColor: 'var(--alert-background-color)',
+  color: 'var(--alert-text-color)',
   [`:where([${DATA_ATTRIBUTES.orientation}="vertical"])`]: {
     '> button[data-dismiss]': {
       alignSelf: 'flex-start',
@@ -53,24 +54,28 @@ const StyledElement = styled('div')({
     },
   },
   [COLORSCHEME_SELECTORS.cyan]: {
+    '--alert-text-color': colors.cyan900,
     '--alert-background-color': colors.cyan50,
     '--alert-icon-color': colors.cyan700,
     '--alert-border-color': colors.cyan500,
     '--alert-focus-color': colors.cyan700,
   },
   [COLORSCHEME_SELECTORS.green]: {
+    '--alert-text-color': colors.green900,
     '--alert-background-color': colors.green50,
     '--alert-icon-color': colors.green700,
     '--alert-border-color': colors.green500,
     '--alert-focus-color': colors.green700,
   },
   [COLORSCHEME_SELECTORS.gold]: {
+    '--alert-text-color': colors.gold900,
     '--alert-background-color': colors.gold50,
     '--alert-icon-color': colors.gold700,
     '--alert-border-color': colors.gold500,
     '--alert-focus-color': colors.gold700,
   },
   [COLORSCHEME_SELECTORS.red]: {
+    '--alert-text-color': colors.red900,
     '--alert-background-color': colors.red50,
     '--alert-icon-color': colors.red700,
     '--alert-border-color': colors.red500,
--- packages/web-ui/src/Alert/AlertText.tsx
+++ packages/web-ui/src/Alert/AlertText.tsx
@@ -1,30 +1,12 @@
 import * as React from 'react';
-import { colors } from '@utilitywarehouse/colour-system';
 import { Text } from '../Text';
 import type { TextProps } from '../Text';
-import { styled } from '../theme';
-import { colorSchemeParentSelector, withGlobalPrefix } from '../utils';
-import { COLOR_SCHEME, PropsWithSx } from '../types';
+import { withGlobalPrefix } from '../utils';
+import { PropsWithSx } from '../types';
 import clsx from 'clsx';
 
 const componentName = 'AlertText';
 const componentClassName = withGlobalPrefix(componentName);
-
-const StyledElement = styled(Text)({
-  color: 'var(--alert-text-color)',
-  [colorSchemeParentSelector(COLOR_SCHEME.cyan)]: {
-    '--alert-text-color': colors.cyan900,
-  },
-  [colorSchemeParentSelector(COLOR_SCHEME.green)]: {
-    '--alert-text-color': colors.green900,
-  },
-  [colorSchemeParentSelector(COLOR_SCHEME.gold)]: {
-    '--alert-text-color': colors.gold900,
-  },
-  [colorSchemeParentSelector(COLOR_SCHEME.red)]: {
-    '--alert-text-color': colors.red900,
-  },
-});
 
 /**
  * An `AlertText` is a component that is used to display the text of an `Alert`.
@@ -35,7 +17,14 @@ export const AlertText = React.forwardRef<
   React.ElementRef<'div'>,
   React.PropsWithChildren<PropsWithSx<TextProps>>
 >(({ className, ...props }, ref) => (
-  <StyledElement ref={ref} className={clsx(componentClassName, className)} {...props} />
+  <Text
+    ref={ref}
+    className={clsx(componentClassName, className)}
+    variant="body"
+    color="inherit"
+    component="p"
+    {...props}
+  />
 ));
 
 AlertText.displayName = componentName;
