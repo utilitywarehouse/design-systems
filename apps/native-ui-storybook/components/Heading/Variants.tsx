8a42a522b2dddc1762680b2951594b5bc2d16e6d
Jord McCord <jmccord@uw.co.uk>
2024-07-04 18:16:08 +0100

    chore: add variant title for docs

--- apps/native-ui-storybook/components/Heading/Variants.tsx
+++ apps/native-ui-storybook/components/Heading/Variants.tsx
@@ -1,38 +1,30 @@
 import React from 'react';
-import { Box, Center, Text, Heading, VStack } from '@utilitywarehouse/native-ui';
-
-const TextWrap = ({ title, children }: any) => (
-  <Box gap="$2">
-    <Text textTransform="uppercase" size="xs" highlight color="$grey600">
-      {title}
-    </Text>
-    {children}
-  </Box>
-);
+import { Center, Heading, VStack } from '@utilitywarehouse/native-ui';
+import { VariantTitle } from '../../docs/components';
 
 const HeadingBasic = () => {
   return (
     <VStack gap="$6">
-      <TextWrap title="Heading 1">
+      <VariantTitle title="Heading 1">
         <Heading size="h1">Aeonik - Bold (700), 32px Size / 40px Line height / 0px Letter</Heading>
-      </TextWrap>
-      <TextWrap title="Heading 2">
+      </VariantTitle>
+      <VariantTitle title="Heading 2">
         <Heading size="h2">Aeonik - Bold (700), 28px Size / 32px Line height / 0px Letter</Heading>
-      </TextWrap>
-      <TextWrap title="Heading 3">
+      </VariantTitle>
+      <VariantTitle title="Heading 3">
         <Heading size="h3">Aeonik - Bold (700), 24px Size / 32px Line height / 0px Letter</Heading>
-      </TextWrap>
-      <TextWrap title="Heading 4">
+      </VariantTitle>
+      <VariantTitle title="Heading 4">
         <Heading size="h4">Aeonik - Bold (700), 18px Size / 24px Line height / 0px Letter</Heading>
-      </TextWrap>
-      <TextWrap title="Heading 5">
+      </VariantTitle>
+      <VariantTitle title="Heading 5">
         <Heading size="h5">Aeonik - Bold (700), 16px Size / 20px Line height / 0px Letter</Heading>
-      </TextWrap>
-      <TextWrap title="Heading 6">
+      </VariantTitle>
+      <VariantTitle title="Heading 6">
         <Heading size="h6">
           Work Sans - Regular (400), 16px Size / 24px Line height / 0px Letter
         </Heading>
-      </TextWrap>
+      </VariantTitle>
     </VStack>
   );
 };
--- apps/native-ui-storybook/components/Text/Variants.tsx
+++ apps/native-ui-storybook/components/Text/Variants.tsx
@@ -1,42 +1,34 @@
 import React from 'react';
-import { Box, Center, Text, VStack } from '@utilitywarehouse/native-ui';
-
-const TextWrap = ({ title, children }: any) => (
-  <Box gap="$2">
-    <Text textTransform="uppercase" size="xs" highlight color="$grey600">
-      {title}
-    </Text>
-    {children}
-  </Box>
-);
+import { Center, Text, VStack } from '@utilitywarehouse/native-ui';
+import { VariantTitle } from '../../docs/components';
 
 const TextBasic = () => {
   return (
     <VStack gap="$6">
-      <TextWrap title="Default - Body / Medium">
+      <VariantTitle title="Default - Body / Medium">
         <Text>Work Sans - Regular (400), 16px Size / 24px Line height / 0px Letter</Text>
-      </TextWrap>
-      <TextWrap title="Default - Body / Medium - Strikethrough">
+      </VariantTitle>
+      <VariantTitle title="Default - Body / Medium - Strikethrough">
         <Text strikeThrough>
           Work Sans - Regular (400), 16px Size / 24px Line height / 0px Letter
         </Text>
-      </TextWrap>
-      <TextWrap title="Default - Body / Medium - Highlight">
+      </VariantTitle>
+      <VariantTitle title="Default - Body / Medium - Highlight">
         <Text highlight>
           Work Sans - Semi Bold (600), 16px Size / 24px Line height / 0px Letter
         </Text>
-      </TextWrap>
-      <TextWrap title="Body Small">
+      </VariantTitle>
+      <VariantTitle title="Body Small">
         <Text size="sm">Work Sans - Regular (400), 14px Size / 16px Line height / 0px Letter</Text>
-      </TextWrap>
-      <TextWrap title="Body Small - Highlighted">
+      </VariantTitle>
+      <VariantTitle title="Body Small - Highlighted">
         <Text size="sm" highlight>
           Work Sans - Semi Bold (600), 14px Size / 16px Line height / 0px Letter
         </Text>
-      </TextWrap>
-      <TextWrap title="Body X-Small">
+      </VariantTitle>
+      <VariantTitle title="Body X-Small">
         <Text size="xs">Work Sans - Regular (400), 12px Size / 16px Line height / 0px Letter</Text>
-      </TextWrap>
+      </VariantTitle>
     </VStack>
   );
 };
