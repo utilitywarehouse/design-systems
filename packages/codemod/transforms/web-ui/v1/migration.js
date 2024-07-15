7478a48f82bda873cce021189168cd9ecf5853c7
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2024-07-15 09:36:16 +0100

    Add codemod to update IconButton size (#421)

--- packages/codemod/transforms/web-ui/v1/migration.js
+++ packages/codemod/transforms/web-ui/v1/migration.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-undef */
 const transformRenameLabButtonSize = require('./rename-lab-button-size');
+const transformRenameIconButtonSize = require('./rename-icon-button-size');
 
 function transformer(file, api, options) {
   // run this before migrating Button out of lab submodule
   file.source = transformRenameLabButtonSize(file, api, options);
+  file.source = transformRenameIconButtonSize(file, api, options);
   return file.source;
 }
 
