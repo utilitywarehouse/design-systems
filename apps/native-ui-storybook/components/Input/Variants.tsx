71e9a9ee1c0129f7b40513dbe2e36827a4096d38
Jord McCord <jmccord@uw.co.uk>
2024-07-16 15:18:36 +0100

    chore: update input docs

--- apps/native-ui-storybook/components/Input/Variants.tsx
+++ apps/native-ui-storybook/components/Input/Variants.tsx
@@ -1,102 +1,114 @@
 import React from 'react';
 import {
-  Box,
   Input,
   InputField,
   InputIcon,
   InputSlot,
   ScrollView,
-  Text,
+  VStack,
 } from '@utilitywarehouse/native-ui';
 import { StoryFn } from '@storybook/react';
 import { EmailMediumIcon } from '@utilitywarehouse/react-native-icons';
+import { VariantTitle } from '../../docs/components';
 
 const InputVariants: StoryFn = () => {
   return (
     <ScrollView>
-      <Box gap={8}>
-        <Text>Defaut</Text>
-        <Input>
-          <InputField />
-        </Input>
-        <Text>With icon</Text>
-        <Input>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField />
-        </Input>
-        <Text>With placeholder</Text>
-        <Input>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" />
-        </Input>
-        <Text>With value</Text>
-        <Input>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" value="filling the value" />
-        </Input>
-        <Text>Focused</Text>
-        <Input isFocused>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" value="filling the value" />
-        </Input>
-        <Text>Type password</Text>
-        <Input>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" value="filling the value" type="password" />
-        </Input>
-        <Text>Valid</Text>
-        <Input validationStatus="valid">
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" />
-        </Input>
-        <Text>Valid focused</Text>
-        <Input validationStatus="valid" isFocused>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" />
-        </Input>
-        <Text>Invalid</Text>
-        <Input validationStatus="invalid">
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" />
-        </Input>
-        <Text>Invalid focused</Text>
-        <Input validationStatus="invalid" isFocused>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" />
-        </Input>
-        <Text>Disabled</Text>
-        <Input isDisabled>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" />
-        </Input>
-        <Text>Readonly</Text>
-        <Input isReadOnly>
-          <InputSlot>
-            <InputIcon as={EmailMediumIcon} />
-          </InputSlot>
-          <InputField placeholder="Input placeholder" readOnly />
-        </Input>
-      </Box>
+      <VStack gap="$4">
+        <VariantTitle title="Default">
+          <Input>
+            <InputField />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="With icon">
+          <Input>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="With placeholder">
+          <Input>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="With value">
+          <Input>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" value="filling the value" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Focused">
+          <Input isFocused>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" value="filling the value" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Type password">
+          <Input>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" value="filling the value" type="password" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Valid">
+          <Input validationStatus="valid">
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Valid focused">
+          <Input validationStatus="valid" isFocused>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Invalid">
+          <Input validationStatus="invalid">
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Invalid focused">
+          <Input validationStatus="invalid" isFocused>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Disabled">
+          <Input isDisabled>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" />
+          </Input>
+        </VariantTitle>
+        <VariantTitle title="Readonly">
+          <Input isReadOnly>
+            <InputSlot>
+              <InputIcon as={EmailMediumIcon} />
+            </InputSlot>
+            <InputField placeholder="Input placeholder" readOnly />
+          </Input>
+        </VariantTitle>
+      </VStack>
     </ScrollView>
   );
 };
