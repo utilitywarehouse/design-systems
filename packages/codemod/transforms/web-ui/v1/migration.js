8e15eb761b39af4a94722fae94a041db7dde65b4
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2024-08-05 15:08:04 +0100

    Add codemod to update `Typography` components (#437)

--- packages/codemod/transforms/web-ui/v1/migration.js
+++ packages/codemod/transforms/web-ui/v1/migration.js
@@ -2,12 +2,14 @@
 const transformUpdateLabImports = require('./update-lab-imports');
 const transformRenameLabButtonSize = require('./rename-lab-button-size');
 const transformRenameIconButtonSize = require('./rename-icon-button-size');
+const transformTypography = require('./typography-to-heading-and-text');
 
 function transformer(file, api, options) {
   file.source = transformUpdateLabImports(file, api, options);
   // run this before migrating Button out of lab submodule
   file.source = transformRenameLabButtonSize(file, api, options);
   file.source = transformRenameIconButtonSize(file, api, options);
+  file.source = transformTypography(file, api, options);
   return file.source;
 }
 
