be2ce6b8d1eaf4dec42cfdce01ca0aa6951e497c
Rob Phoenix <rphoenix@uw.co.uk>
2024-08-14 12:40:52 +0100

    fix migration codemods order

--- packages/codemod/transforms/web-ui/v1/migration.js
+++ packages/codemod/transforms/web-ui/v1/migration.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-undef */
-const transformUpdateLabImports = require('./update-lab-imports');
 const transformRenameLabButtonSize = require('./rename-lab-button-size');
 const transformRenameIconButtonSize = require('./rename-icon-button-size');
+const transformUpdateLabImports = require('./update-lab-imports');
 const transformTypography = require('./typography-to-heading-and-text');
 
 function transformer(file, api, options) {
-  file.source = transformUpdateLabImports(file, api, options);
-  // run this before migrating Button out of lab submodule
-  file.source = transformRenameLabButtonSize(file, api, options);
+  file.source = transformRenameLabButtonSize(file, api, options); // run this before migrating Button out of lab submodule
   file.source = transformRenameIconButtonSize(file, api, options);
+  file.source = transformUpdateLabImports(file, api, options);
   file.source = transformTypography(file, api, options);
   return file.source;
 }
