967734a5348ecdef99efa2119dff147610bd76e6
Jord McCord <jmccord@uw.co.uk>
2024-08-15 11:36:08 +0100

    chore: update deps

--- apps/native-ui-storybook/babel.config.js
+++ apps/native-ui-storybook/babel.config.js
@@ -8,14 +8,11 @@ module.exports = function (api) {
         'module-resolver',
         {
           alias: {
-            '@utilitywarehouse/native-ui': path.join(__dirname, '../../packages/native-ui/src'),
-            '@utilitywarehouse/native-ui/lab': path.join(
-              __dirname,
-              '../../packages/native-ui/src/lab'
-            ),
+            '@utilitywarehouse/native-ui': path.join(__dirname, '../../packages/native-ui'),
+            '@utilitywarehouse/native-ui/lab': path.join(__dirname, '../../packages/native-ui/lab'),
             '@utilitywarehouse/native-ui/unstyled': path.join(
               __dirname,
-              '../../packages/native-ui/src/components/unstyled'
+              '../../packages/native-ui/unstyled'
             ),
           },
         },
