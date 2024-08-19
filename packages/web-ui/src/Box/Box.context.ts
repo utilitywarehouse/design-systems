d5d09c56fabb5ce033d1b53c5b5cb0bfc0c6324c
Rob Phoenix <rphoenix@uw.co.uk>
2024-08-19 15:06:41 +0100

    remove isBrandBackground from Box context

--- packages/web-ui/src/Box/Box.context.ts
+++ packages/web-ui/src/Box/Box.context.ts
@@ -3,14 +3,11 @@ import { colorsCommon } from '@utilitywarehouse/colour-system';
 
 type BackgroundContextValue = {
   background: string;
-  /** @deprecated Please use `isInvertedBackground` */
-  isBrandBackground: boolean;
   isInvertedBackground: boolean;
 };
 
 const BackgroundContext = createContext<BackgroundContextValue>({
   background: colorsCommon.brandWhite,
-  isBrandBackground: false,
   isInvertedBackground: false,
 });
 
