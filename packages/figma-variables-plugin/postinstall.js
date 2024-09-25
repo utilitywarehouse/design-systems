0b174e77c23bb3ade82614e98a28df5ff294c3c2
Jord McCord <jmccord@uw.co.uk>
2024-09-25 15:20:27 +0100

    fix: postinstall

--- packages/figma-variables-plugin/postinstall.js
+++ packages/figma-variables-plugin/postinstall.js
@@ -2,9 +2,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const path = require('path');
+const fs = require('fs');
 const { execSync } = require('child_process');
+
 const pluginTypingsSrc = path.resolve(__dirname, '../../node_modules/@figma/plugin-typings');
 const pluginTypingsDest = path.resolve(__dirname, './node_modules/@figma/plugin-typings');
+
 const eslintPluginSrc = path.resolve(
   __dirname,
   '../../node_modules/@figma/eslint-plugin-figma-plugins'
@@ -14,11 +17,13 @@ const eslintPluginDest = path.resolve(
   './node_modules/@figma/eslint-plugin-figma-plugins'
 );
 
-try {
-  execSync(`cp -R ${pluginTypingsSrc} ${pluginTypingsDest}`);
-  execSync(`cp -R ${eslintPluginSrc} ${eslintPluginDest}`);
-} catch (error) {
-  if (error.status === 1) {
-    console.log('Error copying plugin-typings or eslint-plugin-figma-plugins');
-  }
-}
+// Ensure the destination directories exist
+const pluginTypingsDestDir = path.dirname(pluginTypingsDest);
+fs.mkdirSync(pluginTypingsDestDir, { recursive: true });
+
+const eslintPluginDestDir = path.dirname(eslintPluginDest);
+fs.mkdirSync(eslintPluginDestDir, { recursive: true });
+
+// Proceed with copying the files
+execSync(`cp -R ${pluginTypingsSrc} ${pluginTypingsDest}`);
+execSync(`cp -R ${eslintPluginSrc} ${eslintPluginDest}`);
