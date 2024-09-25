8ba91ac4f05f38cadddd11612c86739dc971a630
Jord McCord <jmccord@uw.co.uk>
2024-09-25 12:17:17 +0100

    feat: figma plugin

--- packages/variables-plugin/build.js
+++
@@ -1,9 +0,0 @@
-import * as esbuild from 'esbuild';
-import envFilePlugin from 'esbuild-envfile-plugin';
-
-await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  outfile: 'dist/index.js',
-  plugins: [envFilePlugin],
-});
---
+++ packages/variables-plugin/postinstall.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const path = require('path');
+const { execSync } = require('child_process');
+const pluginTypingsSrc = path.resolve(__dirname, '../../node_modules/@figma/plugin-typings');
+const pluginTypingsDest = path.resolve(__dirname, './node_modules/@figma/plugin-typings');
+const eslintPluginSrc = path.resolve(__dirname, '../../node_modules/@figma/eslint-plugin-figma-plugins');
+const eslintPluginDest = path.resolve(__dirname, './node_modules/@figma/eslint-plugin-figma-plugins');
+
+execSync(`cp -R ${pluginTypingsSrc} ${pluginTypingsDest}`);
+execSync(`cp -R ${eslintPluginSrc} ${eslintPluginDest}`);
