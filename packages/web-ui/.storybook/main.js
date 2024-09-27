20bdabefbb51a10af138b59d215402aae804c0e5
Rob Phoenix <rphoenix@uw.co.uk>
2024-09-27 13:33:47 +0100

    maybe maybe

--- packages/web-ui/.storybook/main.js
+++ packages/web-ui/.storybook/main.js
@@ -1,11 +1,12 @@
+import { dirname, join } from "path";
 import remarkGfm from 'remark-gfm';
 
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   stories: ['../docs/**/*.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-a11y"),
     {
       name: '@storybook/addon-essentials',
       options: {
@@ -22,14 +23,14 @@ const config = {
         },
       },
     },
+    getAbsolutePath("@storybook/addon-webpack5-compiler-babel")
   ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: {},
   },
   docs: {
-    defaultName: 'Documentation',
-    autodocs: 'tag',
+    defaultName: 'Documentation'
   },
   typescript: {
     reactDocgenTypescriptOptions: {},
@@ -37,3 +38,7 @@ const config = {
   staticDirs: ['static'],
 };
 export default config;
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
--- packages/web-ui/.storybook/preview.js
+++ packages/web-ui/.storybook/preview.js
@@ -1,11 +1,11 @@
-import '@utilitywarehouse/fontsource';
-import '@utilitywarehouse/css-reset';
-import { breakpoints, Box } from '../src';
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-import { colorsCommon } from '@utilitywarehouse/colour-system';
-import { SyntaxHighlighter } from '@storybook/components';
-import diff from 'react-syntax-highlighter/dist/esm/languages/prism/diff';
 import { Analytics } from '@vercel/analytics/react';
+import diff from 'react-syntax-highlighter/dist/esm/languages/prism/diff';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { SyntaxHighlighter } from '@storybook/components';
+import { colorsCommon } from '@utilitywarehouse/colour-system';
+import '@utilitywarehouse/css-reset';
+import '@utilitywarehouse/fontsource';
+import { breakpoints, Box } from '../src';
 
 SyntaxHighlighter.registerLanguage('diff', diff);
 
@@ -66,7 +66,6 @@ const preview = {
     viewport: {
       viewports: customerUiViewports,
     },
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
