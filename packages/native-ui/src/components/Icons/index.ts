88a905d665b66b4f4ab7f00ea526fd7f637bb4ff
Rob Phoenix <rphoenix@uw.co.uk>
2024-09-27 12:45:25 +0100

    ugh

--- packages/native-ui/src/components/Icons/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CircleIcon } from './CircleIcon';
--- packages/native-ui/src/components/Radio/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  default as Radio,
-  RadioGroup as RadioGroup,
-  RadioIndicator as RadioIndicator,
-  RadioLabel as RadioLabel,
-  RadioIcon as RadioIcon,
-} from './Radio';
