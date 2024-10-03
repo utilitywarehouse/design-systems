8572942f7b192ef75ca25b25eb00e726607ee631
Rob Phoenix <rphoenix@uw.co.uk>
2024-10-03 09:47:29 +0100

    add Alert code connect

---
+++ packages/web-ui/src/components/Alert/Alert.figma.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import figma from '@figma/code-connect';
+
+import { Alert } from './Alert';
+
+figma.connect(
+  Alert,
+  'https://www.figma.com/design/4FFYTLWJ2hQpj36JplQQUw/UW-Web-UI?node-id=5448-21854&m=dev',
+  {
+    props: {
+      title: figma.string('title'),
+      link: figma.string('link'),
+      text: figma.string('text'),
+      close: figma.boolean('close', {
+        true: () => console.log('close'),
+        false: undefined,
+      }),
+      colorScheme: figma.enum('colorScheme', {
+        red: 'red',
+        gold: 'gold',
+        cyan: 'cyan',
+        green: 'green',
+      }),
+      direction: figma.enum('direction', {
+        column: 'column',
+        row: 'row',
+      }),
+    },
+    example: ({ title, text, link, close, colorScheme, direction }) => (
+      <Alert
+        title={title}
+        text={text}
+        linkText={link}
+        onClose={close}
+        colorScheme={colorScheme}
+        direction={direction}
+      />
+    ),
+  }
+);
