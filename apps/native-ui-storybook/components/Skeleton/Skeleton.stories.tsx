c5f779a184555986c3fe5dda4bfef6e23e04205f
Jord McCord <jmccord@uw.co.uk>
2024-10-14 15:01:02 +0100

    chore: add `Skeleton` docs

---
+++ apps/native-ui-storybook/components/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,11 @@
+import Skeleton from './Skeleton';
+import type { Meta } from '@storybook/react';
+
+const SkeletonMeta: Meta<typeof Skeleton> = {
+  title: 'Native UI / Components / Skeleton',
+  component: Skeleton,
+};
+
+export default SkeletonMeta;
+
+export { Skeleton };
