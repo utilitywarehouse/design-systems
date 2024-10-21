0fec25a5e4643e17b80db4243a4890dca117a74b
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2024-10-21 16:42:28 +0100

    fix checkbox code connect (#579)

--- packages/web-ui/src/components/CheckboxTile/CheckboxTile.figma.tsx
+++ packages/web-ui/src/components/CheckboxTile/CheckboxTile.figma.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 import figma from '@figma/code-connect';
 
-import { Checkbox } from './Checkbox';
+import { CheckboxTile } from './CheckboxTile';
 
 figma.connect(
-  Checkbox,
-  'https://www.figma.com/design/4FFYTLWJ2hQpj36JplQQUw/UW-Web-UI?node-id=5300-21339&m=dev',
+  CheckboxTile,
+  'https://www.figma.com/design/4FFYTLWJ2hQpj36JplQQUw/UW-Web-UI?node-id=5300-21487&m=dev',
   {
     props: {
-      disabled: figma.boolean('disabled'),
       label: figma.nestedProps('Label', {
         text: figma.string('Label Text'),
       }),
       helperText: figma.nestedProps('Helper Text', {
         text: figma.string('Text'),
       }),
+      disabled: figma.enum('state', {
+        disabled: true,
+      }),
     },
     example: ({ label, helperText, disabled }) => (
-      <Checkbox label={label.text} helperText={helperText.text} disabled={disabled} />
+      <CheckboxTile label={label.text} helperText={helperText.text} disabled={disabled} />
     ),
   }
 );
