a4ddc32e499b2abb77af8eb4a15dd26874608e07
Jord McCord <jmccord@uw.co.uk>
2024-10-23 11:16:01 +0100

    chore: figma code connect

--- packages/native-ui/src/components/Radio/Radio.figma.tsx
+++ packages/native-ui/src/components/Radio/Radio.figma.tsx
@@ -10,16 +10,34 @@ figma.connect(
   'https://www.figma.com/design/3RY3OvLA88yZksRjOfjQJm/UW-App-UI?node-id=5626-1463&m=dev',
   {
     props: {
-      helperText: figma.boolean('Helper Text?'),
       disabled: figma.boolean('disabled'),
-      invalid: figma.boolean('invalid'),
       label: figma.nestedProps('Label', {
         text: figma.string('Label Text'),
       }),
+      helperText: figma.nestedProps('Helper Text', {
+        text: figma.string('Text'),
+      }),
+      invalidText: figma.boolean('invalid', {
+        true: figma.nestedProps('Helper Text', {
+          text: figma.string('Text'),
+          showIcon: figma.boolean('showIcon?'),
+        }),
+        false: {
+          text: undefined,
+          showIcon: undefined,
+        },
+      }),
     },
-    example: ({ disabled, label }) => (
+    example: ({ disabled, label, helperText, invalidText }) => (
       <RadioGroup value={value} onChange={setValue}>
-        <Radio value="someValue" disabled={disabled} label={label.text} />
+        <Radio
+          value="someValue"
+          disabled={disabled}
+          label={label.text}
+          helperText={helperText.text}
+          invalidText={invalidText.text}
+          showValidationIcon={invalidText.showIcon}
+        />
       </RadioGroup>
     ),
   }
