43bf10cf2a26c440cfae10513e751675dac53d9d
Jord McCord <jmccord@uw.co.uk>
2024-11-28 16:45:41 +0000

    fix: useMedia

--- packages/native-ui/src/hooks/useMedia.ts
+++ packages/native-ui/src/hooks/useMedia.ts
@@ -1,31 +1,28 @@
-import { useStyles } from 'react-native-unistyles';
+import { useStyles, UnistylesRuntime } from 'react-native-unistyles';
 
 const useMedia = (): Partial<Record<keyof typeof breakpoints, boolean>> => {
-  const {
-    theme: { breakpoints },
-    breakpoint,
-  } = useStyles();
+  const { breakpoint } = useStyles();
+  const breakpoints = UnistylesRuntime.breakpoints;
 
   type BreakPointKeys = keyof typeof breakpoints;
   const mediaBreakpoints: Partial<Record<BreakPointKeys, boolean>> = {};
 
   // Type guard function to check if 'breakpoint' is a key of 'breakpoints'
-  // function isBreakpointKey(
-  //   value: keyof UnistylesBreakpoints,
-  //   breakpointsObj: typeof breakpoints
-  // ): value is BreakPointKeys {
-  //   return value in breakpointsObj;
-  // }
+  function isBreakpointKey(
+    value: BreakPointKeys,
+    breakpointsObj: typeof breakpoints
+  ): value is BreakPointKeys {
+    return value in breakpointsObj;
+  }
 
-  // if (isBreakpointKey(breakpoint, breakpoints)) {
-  if (breakpoint in breakpoints) {
+  if (isBreakpointKey(breakpoint, breakpoints)) {
     (Object.keys(breakpoints) as Array<BreakPointKeys>).forEach(currentBreakPoint => {
       mediaBreakpoints[currentBreakPoint] =
         breakpoints[currentBreakPoint] === breakpoints[breakpoint];
     });
   } else {
     // Handle the case where 'breakpoint' is not a valid key
-    throw new Error(`Invalid breakpoint: ${breakpoint}`);
+    throw new Error(`Invalid breakpoint: ${String(breakpoint as string)}`);
   }
 
   return mediaBreakpoints;
