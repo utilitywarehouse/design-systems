555585bddf9b23d1a76d63e0dd5d4cf0eba5a80e
Jord McCord <jmccord@uw.co.uk>
2025-01-16 17:16:35 +0000

    feat: include breakpoint exports

--- packages/native-ui/src/core/index.ts
+++ packages/native-ui/src/core/index.ts
@@ -2,4 +2,7 @@ export { StyleSheet } from 'react-native-unistyles';
 
 export { default as NativeUIProvider } from './NativeUIProvider';
 
+export { appThemes as themes } from './themes';
+export { breakpoints } from './breakpoints';
+
 export type * from '../types/unistyles';
