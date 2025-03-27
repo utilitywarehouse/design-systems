a51b007c1f05656d9eaf482576da063b8b348ac7
Jord McCord <jmccord@uw.co.uk>
2025-03-27 18:48:01 +0000

    fix: prod build

--- packages/native-ui/vite.config.ts
+++ packages/native-ui/vite.config.ts
@@ -1,26 +1,34 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactNativeWeb from 'vite-plugin-react-native-web';
+import babel from 'vite-plugin-babel';
+import { defineConfig } from 'vite';
 
-/** @type {import('react-native-unistyles/plugin').UnistylesPluginOptions} */
 const unistylesPluginOptions = {
-  autoProcessImports: ['@utilitywarehouse/hearth-react-native'],
+  autoProcessImports: ['@utilitywarehouse/native-ui'],
   // autoProcessPaths: ['../../src'],
   debug: false,
 };
 
-// https://vite.dev/config/
+const extensions = [
+  '.web.tsx',
+  '.tsx',
+  '.web.ts',
+  '.ts',
+  '.web.jsx',
+  '.jsx',
+  '.web.js',
+  '.js',
+  '.css',
+  '.json',
+  '.mjs',
+];
+
+const development = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@utilitywarehouse/react-native-icons': '@utilitywarehouse/react-icons',
-    },
-  },
+  clearScreen: true,
   plugins: [
-    reactNativeWeb(),
     react({
       babel: {
-        presets: ['@babel/preset-react'],
         plugins: [
           ['react-native-unistyles/plugin', unistylesPluginOptions],
           '@babel/plugin-proposal-export-namespace-from',
@@ -28,5 +36,47 @@ export default defineConfig({
         ],
       },
     }),
+    ...(development
+      ? []
+      : [
+          babel({
+            include: [/node_modules\/(react-native|@react-native)/],
+            babelConfig: {
+              plugins: [
+                [
+                  // this is a fix for reanimated not working in production
+                  '@babel/plugin-transform-modules-commonjs',
+                  {
+                    strict: false,
+                    strictMode: false, // prevent "use strict" injections
+                    allowTopLevelThis: true, // dont rewrite global `this` -> `undefined`
+                  },
+                ],
+              ],
+            },
+          }),
+        ]),
   ],
+  define: {
+    __DEV__: JSON.stringify(development),
+    DEV: JSON.stringify(development),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'global.__x': {},
+    _frameTimestamp: undefined,
+    _WORKLET: false,
+  },
+  resolve: {
+    extensions,
+    alias: {
+      'react-native': 'react-native-web',
+      '@utilitywarehouse/react-native-icons': '@utilitywarehouse/react-icons',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: extensions,
+      jsx: 'automatic',
+      loader: { '.js': 'jsx' },
+    },
+  },
 });
