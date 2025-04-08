dbb42db3d346a9c76040cf0e920ceac7b8a56509
Jord McCord <jmccord@uw.co.uk>
2025-04-08 12:19:54 +0100

    fix: typo and add eas-cli [skip-native]

--- packages/design-tokens/build/js/index.js
+++
@@ -1,5 +0,0 @@
-import * as colors from './colors';
-import * as components from './components';
-import * as primitive from './primitive';
-import * as semantic from './semantic';
-export { colors, components, primitive, semantic };
