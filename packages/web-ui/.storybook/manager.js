61f9c2685abfe105956bfee6c083c2c627f5e508
Jord McCord <jmccord@uw.co.uk>
2025-07-09 15:57:30 +0100

    fix: storybook build

--- packages/web-ui/.storybook/manager.js
+++ packages/web-ui/.storybook/manager.js
@@ -1,6 +1,6 @@
-import { addons } from '@storybook/addons';
-import { create } from 'storybook/theming';
 import { version } from '../package.json';
+import { addons } from 'storybook/manager-api';
+import { create } from 'storybook/theming';
 
 const theme = create({
   brandTitle: `UW Web UI v${version}`,
