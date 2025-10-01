5663bc9d9c196097f6dcfd69c4dea4190ac0bfa8
Mark Head <mickeyhead7@gmail.com>
2025-10-01 20:43:33 +0100

    fix: fixes lint error

--- packages/native-ui/src/lab/Carousel/Carousel.props.ts
+++ packages/native-ui/src/lab/Carousel/Carousel.props.ts
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
-import { FlatList, FlatListProps, ViewProps, ViewStyle } from 'react-native';
+import { FlatList, FlatListProps, ViewProps } from 'react-native';
 
 export interface CarouselRef extends FlatList {}
 
